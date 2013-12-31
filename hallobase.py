149c1bea572c54600b96c39223fbb0185c0a6819
dr-spangle <dr-spangle@dr-spangle.com>
2013-12-31 17:43:33 +0000

    added random card function

--- hallobase.py
+++ hallobase.py
@@ -925,6 +925,28 @@ _ #' _(_-'_()\ \" | ,-6-_,--' | / "" L-'\ \,--^---v--v-._ / \ |
         else:
             return "You do not have sufficient privileges to run this command."
         
+    def fnn_randcard(self,exclude_cards):
+        'Picks a random card from a deck, excluding those given to it in a list'
+        deck = [y + str(x).replace('11','j').replace('12','q').replace('13','k') for y in ['s','c','d','h'] for x in range(1,14)]
+        for card in exclude_cards:
+            deck.remove(card)
+        num_cards = len(deck)
+        rand_card = random.randint(0,num_cards)
+        return deck[rand_card]
+
+    def fn_random_card(self,args,client,destination):
+        'Picks a random card from a deck.'
+        card = hallobase.fnn_randcard(self,[])
+        carddesc = card[1:].replace('k','King').replace('q','Queen').replace('j','Jack') + " of "
+        if(card[0]=='c'):
+            carddesc = carddesc + "clubs"
+        elif(card[0]=='s'):
+            carddesc = carddesc + "spades"
+        elif(card[0]=='d'):
+            carddesc = carddesc + "diamonds"
+        else:
+            carddesc = carddesc + "hearts"
+        return "I have chosen the " + carddesc + "."
 
 #    def fn_listadd(self, args, client, destination):
 #        'Creates a new list for this user'
