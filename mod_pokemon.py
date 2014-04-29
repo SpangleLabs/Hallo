dab7fda0e93b1e50bc626ce429f43c3749177929
dr-spangle <dr-spangle@dr-spangle.com>
2014-04-29 11:09:05 +0100

    fixed fn_song and similar

--- mod_pokemon.py
+++ mod_pokemon.py
@@ -7,7 +7,7 @@ class mod_pokemon:
     def fn_i_choose_you(self,args,client,destination):
         'Picks a random pokemon from generation I to generation IV unless a generation is specified. Format: i_choose_you gen <generation>'
         pokemon = pickle.load(open('store/pokemon.p','rb'))
-        randmon = pokemon[random.randint(0,len(pokemon))+1]
+        randmon = pokemon[random.randint(1,len(pokemon))]
         return "I choose you, " + randmon['Name'] + "!"
 
     def fn_i(self,args,client,destination):
@@ -19,3 +19,21 @@ class mod_pokemon:
         else:
             return "Huh?"
 
+    def fn_pokedex(self,args,client,destination):
+        'Returns a random pokedex entry for a given pokemon.'
+        args = args.lower()
+        pokemon = pickle.load(open('store/pokemon.p','rb'))
+        for entry in pokemon:
+            if(pokemon[entry]['Name'].lower() == args):
+                pokedex = pokemon[entry]['Dex_entries']
+                break
+        pokedex_entries = []
+        for animedexentry in pokedex['Anime']:
+            if(pokedex['Anime'][animedexentry]):
+                pokedex_entries.append(pokedex['Anime'][animedexentry])
+        for gamedexentry in pokedex['Games']:
+            if(pokedex['Games'][gamedexentry]):
+                pokedex_entries.append(pokedex['Games'][gamedexentry])
+        if(len(pokedex_entries)==0):
+            return "No available pokedex data."
+        return pokedex_entries[random.randint(0,len(pokedex_entries)-1)]
