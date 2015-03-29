985819b981281d72c2a4eebb51f88cc7f9e76aa1
dr-spangle <dr-spangle@dr-spangle.com>
2015-03-29 23:11:46 +0100

    Adding class variables and ServerIRC object

--- Server.py
+++ Server.py
@@ -4,13 +4,17 @@ class Server(object):
     '''
     Generic server object. An interface for ServerIRC or ServerSkype or whatever objects.
     '''
-    mOpen = False
-    mChannelList = []
-    mUserList = []
-    mConnection = None
-    mNick = None
-    mPrefix = None
-    mFullName = None
+    #Persistent/saved class variables
+    mName = None                #server name
+    mAutoConnect = True         #Whether to automatically connect to this server when hallo starts
+    mChannelList = []           #list of channels on this server (which may or may not be currently active)
+    mConnection = None          #Connection for the server, socket or whatnot
+    mNick = None                #Nickname to use on this server
+    mPrefix = None              #Prefix to use with functions on this server
+    mFullName = None            #Full name to use on this server
+    #Dynamic/unsaved class variables
+    mOpen = False               #Whether or not to keep reading from server
+    mUserList = []              #Users on this server
 
     def __init__(self, params):
         '''
@@ -40,3 +44,50 @@ class Server(object):
         '''
         Returns an XML representation of the server object
         '''
+        
+class ServerIRC(Server):
+    #Persistent/saved class variables
+    mName = None                #server name
+    mAutoConnect = True         #Whether to automatically connect to this server when hallo starts
+    mChannelList = []           #list of channels on this server (which may or may not be currently active)
+    mConnection = None          #Connection for the server, socket or whatnot
+    mNick = None                #Nickname to use on this server
+    mPrefix = None              #Prefix to use with functions on this server
+    mFullName = None            #Full name to use on this server
+    #Dynamic/unsaved class variables
+    mOpen = False               #Whether or not to keep reading from server
+    mUserList = []              #Users on this server
+    #IRC specific variables
+    mServerAddress = None       #Address to connect to server
+    mServerPort = None          #Port to connect to server
+    mNickservPass = None        #Password to identify with nickserv
+    
+    def __init__(self, params):
+        '''
+        Constructor for server object
+        '''
+        raise NotImplementedError
+    
+    def connect(self):
+        raise NotImplementedError
+    
+    def disconnect(self):
+        raise NotImplementedError
+    
+    def run(self):
+        '''
+        Method to read from stream and process. Will call an internal parsing method or whatnot
+        '''
+        raise NotImplementedError
+
+    @staticmethod
+    def fromXml(xmlString):
+        '''
+        Constructor to build a new server object from xml
+        '''
+        
+    def toXml(self):
+        '''
+        Returns an XML representation of the server object
+        '''
+        
