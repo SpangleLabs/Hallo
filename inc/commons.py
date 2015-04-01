a8337625be973791faca1be269abdc6311f973e2
dr-spangle <dr-spangle@dr-spangle.com>
2015-04-01 08:11:40 +0100

    Added a chunkString method, to break a string into a list of strings, each with a max length.

--- inc/commons.py
+++ inc/commons.py
@@ -12,4 +12,8 @@ class Commons(object):
         Constructor
         '''
         return "[{:02d}:{:02d}:{:02d}]".format(*time.gmtime()[3:6])
-        
+
+    @staticmethod    
+    def chunkString(string, length):
+        return (string[0+i:length+i] for i in range(0, len(string), length))
+    
