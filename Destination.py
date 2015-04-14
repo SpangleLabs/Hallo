67ad6952de052e5861157648886668e3b8e139bc
dr-spangle <dr-spangle@dr-spangle.com>
2015-04-14 23:55:50 +0100

    Started work on Destination object, from which Channel and User will inherit.

---
+++ Destination.py
@@ -0,0 +1,12 @@
+
+
+class Destination:
+    '''
+    Destination is an object that both Channel and User inherit from. It just means messages can be sent to these entities.
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
