9cc6b763e72786330fa96aafb40042e5d2e8d5c5
dr-spangle <dr-spangle@dr-spangle.com>
2015-04-14 23:58:15 +0100

    Destination object needs am mName parameter. Set in constructor

--- Destination.py
+++ Destination.py
@@ -4,11 +4,13 @@ class Destination:
     '''
     Destination is an object that both Channel and User inherit from. It just means messages can be sent to these entities.
     '''
+    mName = None
 
-    def __init__(self, params):
+    def __init__(self,name):
         '''
         Constructor
         '''
+        self.mName = name
         
         
 class Channel(Destination):
