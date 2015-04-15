dd5f1dee37295ff3757bc250dde3cdfc375be2ce
dr-spangle <dr-spangle@dr-spangle.com>
2015-04-15 07:39:56 +0100

    Added isChannel() and isUser() to Destination

--- Destination.py
+++ Destination.py
@@ -5,6 +5,7 @@ class Destination:
     Destination is an object that both Channel and User inherit from. It just means messages can be sent to these entities.
     '''
     mName = None
+    mType = None
 
     def __init__(self,name):
         '''
@@ -20,7 +21,26 @@ class Destination:
         'Name setter'
         self.mName = name.lower()
         
+    def getType(self):
+        'Returns whether the destination is a user or channel.'
+        return self.mType
+        
+    def isChannel(self):
+        'Boolean, whether the destination is a channel.'
+        if(self.mType=="channel"):
+            return True
+        else:
+            return False
+        
+    def isUser(self):
+        'Boolean, whether the destination is a user.'
+        if(self.mType=="channel"):
+            return False
+        else:
+            return True
+
 class Channel(Destination):
+    mType = "channel"
     def __init__(self, params):
         '''
         Constructor
@@ -28,6 +48,7 @@ class Channel(Destination):
     
     
 class User(Destination):
+    mType = "user"
     def __init__(self, params):
         '''
         Constructor
