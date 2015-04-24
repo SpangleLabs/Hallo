32b985cf7b6535053f4bfd6135ed149384415183
dr-spangle <dr-spangle@dr-spangle.com>
2015-04-24 12:10:01 +0100

    Implemented dice rolls

--- modules/Random.py
+++ modules/Random.py
@@ -24,19 +24,31 @@ class Roll(Function):
         diceFormatRegex = re.compile("^[0-9]+d[0-9]+$",re.IGNORECASE)
         rangeFormatRegex = re.compile("^[0-9]+-[0-9]+$",re.IGNORECASE)
         if(diceFormatRegex.match(line)):
-            numDice = line.lower().split('d')[0]
-            numSides = line.lower().split('d')[1]
+            numDice = int(line.lower().split('d')[0])
+            numSides = int(line.lower().split('d')[1])
             return self.runDiceFormat(numDice,numSides)
         elif(rangeFormatRegex.match(line)):
-            rangeMin = min(line.split('-'))
-            rangeMax = max(line.split('-'))
+            rangeMin = min([int(x) for x in line.split('-')])
+            rangeMax = max([int(x) for x in line.split('-')])
             return self.runRangeFormat(rangeMin,rangeMax)
         else:
             return "Please give input in the form of X-Y or XdY."
     
     def runDiceFormat(self,numDice,numSides):
         'Rolls numDice number of dice, each with numSides number of sides'
-        pass
+        if(numDice==0 or numDice>100):
+            return "Invalid number of dice."
+        if(numSides==0 or numSides>1000000):
+            return "Invalid number of sides."
+        if(numDice==1):
+            rand = random.randint(1,numSides)
+            return "I roll "+str(rand)+"!!!"
+        else:
+            diceRolls = [random.randint(1,numSides) for _ in range(numDice)]
+            outputString = "I roll "
+            outputString += ", ".join([str(x) for x in diceRolls])
+            outputString += ". The total is " + str(sum(diceRolls)) + "."
+            return outputString
     
     def runRangeFormat(self,rangeMin,rangeMax):
         'Generates a random number between rangeMin and rangeMax'
