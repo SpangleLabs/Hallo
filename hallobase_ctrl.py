4c2012a5c882fb9fcfb754a61c1cca213205dea4
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-02 01:08:42 +0100

    Deprecated and removed fn_connect()

--- hallobase_ctrl.py
+++ hallobase_ctrl.py
@@ -1,9 +1,5 @@
 import ircbot_chk
-from threading import Thread
 import collections
-import re
-
-from Server import ServerIRC
 
 endl = '\r\n'
 class hallobase_ctrl:
@@ -34,44 +30,6 @@ class hallobase_ctrl:
                 return "That message contains a word which is on the swearlist for that channel."
             else:
                 return "Messages sent to " + str(len([destpair for destpair in destlist if self.conf['server'][destpair[0]]['channel'][destpair[1]]['in_channel']])-skipped) + " channels: " + ', '.join(["{" + destpair[0] + "," + destpair[1] + "}" for destpair in destlist if self.conf['server'][destpair[0]]['channel'][destpair[1]]['in_channel']]) + " But not sent to " + str(skipped) + " channels, due to swearlist violation: " + ', '.join(["{" + destpair[0] + "," + destpair[1] + "}" for destpair in destlist if self.conf['server'][destpair[0]]['channel'][destpair[1]]['in_channel']])
-
-    def fn_connect(self,args,client,destination):
-        'Connects to a new server. Requires godmode'
-        if(ircbot_chk.ircbot_chk.chk_god(self,destination[0],client)):
-            args = args.lower()
-            if(':' in args):
-                argsplit = args.split(':')
-                serverPort = int(argsplit[1])
-                serverAddress = argsplit[0]
-            else:
-                serverPort = 6667
-                serverAddress = args
-            serverMatch = re.match(r'([a-z\d\.-]+\.)?([a-z\d-]{1,63})\.([a-z]{2,3}\.[a-z]{2}|[a-z]{2,6})',serverAddress,re.I)
-            serverName = serverMatch.group(2)
-            serverObject = self.getServerByName(serverName)
-            if(serverObject is None):
-                serverNew = ServerIRC(self,serverName,serverAddress,serverPort)
-                self.addServer(serverNew)
-                Thread(target=serverNew.run).start()
-                #TODO: remove all this
-                self.conf['server'][serverName] = {}
-                self.conf['server'][serverName]['ops'] = list(self.conf['server'][destination[0]]['ops'])
-                self.conf['server'][serverName]['gods'] = list(self.conf['server'][destination[0]]['gods'])
-                self.conf['server'][serverName]['address'] = args
-#               self.conf['server'][serverName]['channels'] = []
-                self.conf['server'][serverName]['nick'] = self.conf['server'][destination[0]]['nick']
-                self.conf['server'][serverName]['full_name'] = self.conf['server'][destination[0]]['full_name']
-                self.conf['server'][serverName]['pass'] = False
-                self.conf['server'][serverName]['port'] = serverPort
-                self.conf['server'][serverName]['channel'] = {}
-                self.conf['server'][serverName]['admininform'] = []
-                self.conf['server'][serverName]['pingdiff'] = 600
-                self.conf['server'][serverName]['connected'] = False
-            else:
-                Thread(target=serverObject.run).start()
-            return "Connected to " + serverAddress + " [" + serverName + "]."
-        else:
-            return "Insufficient privileges to connect to a new server."
 
     def fn_help(self,args,client,destination):
         'Gives information about commands.  Use "help commands" for a list of commands, or "help <command>" for help on a specific command.'
