188847439971f7e92ee9ae5245bc084687cdbd15
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-04 19:20:16 +0100

    Added new Pokedex Function, deprecating the last of mod_pokemon

--- modules/Pokemon.py
+++ modules/Pokemon.py
@@ -24,7 +24,7 @@ class RandomPokemon(Function):
         doc = minidom.parse("store/pokemon/pokemon.xml")
         pokemonListXml = doc.getElementsByTagName("pokemon_list")[0]
         pokemonList = []
-        #Loop through pony episodes, adding to episodeList (or songList, if there is a song)
+        #Loop through pokemon, adding to pokemonList
         for pokemonXml in pokemonListXml.getElementsByTagName("pony_episode"):
             pokemonDict = {}
             pokemonDict['name'] = pokemonXml.getElementsByTagName("name")[0].firstChild.data
@@ -54,7 +54,7 @@ class PickATeam(Function):
         doc = minidom.parse("store/pokemon/pokemon.xml")
         pokemonListXml = doc.getElementsByTagName("pokemon_list")[0]
         pokemonList = []
-        #Loop through pony episodes, adding to episodeList (or songList, if there is a song)
+        #Loop through pokemon, adding to pokemonList
         for pokemonXml in pokemonListXml.getElementsByTagName("pony_episode"):
             pokemonDict = {}
             pokemonDict['name'] = pokemonXml.getElementsByTagName("name")[0].firstChild.data
@@ -64,14 +64,14 @@ class PickATeam(Function):
 
 class FullyEvolvedTeam(Function):
     '''
-    Function to select a random pokemon team
+    Function to select a random fully evolved pokemon team
     '''
     #Name for use in help listing
     mHelpName = "fully evolved team"
     #Names which can be used to address the function
     mNames = set(["pick a fully evolved team","fully evolved team","fullyevolvedteam"])
     #Help documentation, if it's just a single line, can be set here
-    mHelpDocs = "Generates a team of pokemon for you."
+    mHelpDocs = "Pick a fully evolved pokemon team."
     
     def __init__(self):
         '''
@@ -84,7 +84,7 @@ class FullyEvolvedTeam(Function):
         doc = minidom.parse("store/pokemon/pokemon.xml")
         pokemonListXml = doc.getElementsByTagName("pokemon_list")[0]
         pokemonList = []
-        #Loop through pony episodes, adding to episodeList (or songList, if there is a song)
+        #Loop through pokemon, adding to pokemonList if they cannot evolve.
         for pokemonXml in pokemonListXml.getElementsByTagName("pony_episode"):
             pokemonDict = {}
             pokemonDict['name'] = pokemonXml.getElementsByTagName("name")[0].firstChild.data
@@ -93,4 +93,47 @@ class FullyEvolvedTeam(Function):
                 pokemonList.append(pokemonDict)
         randomPokemonTeam = random.sample(pokemonList,6)
         return "Your team is: " + ", ".join([pokemon['Name'] for pokemon in randomPokemonTeam[:5]]) + " and " + randomPokemonTeam[5]['Name'] + "."
+
+class Pokedex(Function):
+    '''
+    Function to return pokedex entries
+    '''
+    #Name for use in help listing
+    mHelpName = "pokedex"
+    #Names which can be used to address the function
+    mNames = set(["pokedex","dex"])
+    #Help documentation, if it's just a single line, can be set here
+    mHelpDocs = "Returns a random pokedex entry for a given pokemon."
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+
+    def run(self,line,userObject,destinationObject=None):
+        lineClean = line.split().lower()
+        #Load XML
+        doc = minidom.parse("store/pokemon/pokemon.xml")
+        pokemonListXml = doc.getElementsByTagName("pokemon_list")[0]
+        #Loop through pokemon, searching for the specified pokemon
+        selectedPokemonXml = None
+        for pokemonXml in pokemonListXml.getElementsByTagName("pony_episode"):
+            pokemonName = pokemonXml.getElementsByTagName("name")[0].firstChild.data
+            pokemonNumber = pokemonXml.getElementsByTagName("dex_number")[0].firstChild.data
+            if(lineClean == pokemonName or lineClean == pokemonNumber):
+                selectedPokemonXml = pokemonXml
+                break
+        #If pokemon couldn't be found, return a message to the user
+        if(selectedPokemonXml is None):
+            return "No available pokedex data."
+        #Select a random pokedex entry
+        pokedexEntryListXml = selectedPokemonXml.getElementsByTagName("dex_entry_list")
+        pokedexEntryXml = random.choice(pokedexEntryListXml.getElementsByTagName("dex_entry"))
+        pokedexEntryText = pokedexEntryXml.firstChild.data
+        return pokedexEntryText
+        
+
+
+
 
