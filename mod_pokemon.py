371cfb2e624f1bd1e43b08d3a23b3c2759508c58
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-04 18:59:35 +0100

    Removing deprecated old pokemon function

--- mod_pokemon.py
+++ mod_pokemon.py
@@ -4,21 +4,6 @@ import random
 
 class mod_pokemon:
 
-    def fn_i_choose_you(self,args,client,destination):
-        'Picks a random pokemon from generation I to generation IV unless a generation is specified. Format: i_choose_you gen <generation>'
-        pokemon = pickle.load(open('store/pokemon.p','rb'))
-        randmon = pokemon[random.randint(1,len(pokemon))]
-        return "I choose you, " + randmon['Name'] + "!"
-
-    def fn_i(self,args,client,destination):
-        'Hacky alias for I_choose_you'
-        args = args.lower()
-        if('you' in args):
-            args = args.split('you')[1]
-            return mod_pokemon.fn_i_choose_you(self,args,client,destination)
-        else:
-            return "Huh?"
-
     def fn_pick_a_team(self,args,client,destination):
         'Generates a team of pokemon for you.'
         args = args.lower()
