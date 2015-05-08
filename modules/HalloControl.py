e34120538f9651fcb098c6d5e5b776039cd13a27
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-08 22:59:45 +0100

    Added ModuleReload Function to HalloControl module

--- modules/HalloControl.py
+++ modules/HalloControl.py
@@ -9,7 +9,7 @@ class ConfigSave(Function):
     #Names which can be used to address the function
     mNames = set(["config save","configsave","save config"])
     #Help documentation, if it's just a single line, can be set here
-    mHelpDocs = "Save the config and pickle it. godmod only."
+    mHelpDocs = "Save the config and pickle it."
     
     def __init__(self):
         '''
@@ -20,3 +20,29 @@ class ConfigSave(Function):
     def run(self,line,userObject,destinationObject=None):
         halloObject = userObject.getServer().getHallo()
         halloObject.saveToXml()
+
+class ModuleReload(Function):
+    '''
+    Reloads a specific function module.
+    '''
+    #Name for use in help listing
+    mHelpName = "module reload"
+    #Names which can be used to address the function
+    mNames = set(["module reload","modulereload","reload module"])
+    #Help documentation, if it's just a single line, can be set here
+    mHelpDocs = "Reloads a specified module."
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+
+    def run(self,line,userObject,destinationObject=None):
+        halloObject = userObject.getServer().getHallo()
+        functionDispatcher = halloObject.getFunctionDispatcher()
+        reloadResult = functionDispatcher.reloadModule(line)
+        if(reloadResult == True):
+            return "Module reloaded."
+        else:
+            return "Failed to reload module."
