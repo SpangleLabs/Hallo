8dd1a58d11ad6ea221b469d822967316d45b9515
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-17 23:42:27 +0100

    Converted fn_finnbot into FinnBot Function in new SillyEtd module

--- hallobase_silly.py
+++ hallobase_silly.py
@@ -25,20 +25,6 @@ class hallobase_silly():
             time.sleep(2)
             return '*clap.*'
 
-    def fn_finnbot(self, args, client, destination):
-        'Simulates a typical finn in conversation. Format: finnbot'
-        ariquotes = ["|:", "After hearing you say that, I don't think we can ever be friends", "Brb, cutting down a forest", "Can't answer, I'm shaving and it'll take all day", "Can't hear you over all this atheism!", "Can this wait until after i've listened to this song 100 times on repeat?", "Could use less degrees", "Don't want to hear it, too busy complaining about the tap water", "Goony goon goon", "Hang on, I have to help some micronationalist", "Hey guys, check out my desktop: http://hallo.dr-spangle.com/DESKTOP.PNG", "If we get into a fight, I'll pick you up and run away", "I happen to be an expert on this subject", "I think I've finished constructing a hate engine", "It's about time for me to play through adom again", "It's kind of hard to type while kneeling", "I wish I could answer, but i'm busy redditing", "*lifeless stare*", "Lol, perl", "Lol, remember when i got eli to play crawl for a week?", "Needs moar haskell", "NP: Bad Religion - whatever song", "Remember that thing we were going to do? Now I don't want to do it", "Smells like Oulu", "Some Rahikkala is getting married, you are not invited", "That blows, but I cannot relate to your situation", "This somehow reminds me of my army days", "Whatever, if you'll excuse me, i'm gonna bike 50 kilometers", "You guys are things that say things", "You're under arrest for having too much fun","I have found a new favourite thing to hate"]
-        ariswearquotes = ["FUCK. FINNISH. PEOPLE!!!", "FUCK MANNERHEIM", "YOU'RE A PERSON OF SHIT"]
-        if(self.conf['server'][destination[0]]['channel'][destination[1]]['sweardetect']):
-            for quote in ariswearquotes:
-                ariquotes.append(quote)
-        numquotes = len(ariquotes)
-        rand = random.randint(0,numquotes-1)
-        quote = ariquotes[rand]
-        if(quote[-1] not in ['.','?','!']):
-            quote = quote + '.'
-        return quote
-
     def fn_time(self, args, client, destination):
         'Current time for a given user. Format: time <username>'
         timestamp = time.time()
---
+++ modules/SillyEtd.py
@@ -0,0 +1,31 @@
+from Function import Function
+import random
+
+class FinnBot(Function):
+    '''
+    Spouts random finnishisms reminiscent of ari
+    '''
+    #Name for use in help listing
+    mHelpName = "finnbot"
+    #Names which can be used to address the function
+    mNames = set(["finnbot","random finnishism"])
+    #Help documentation, if it's just a single line, can be set here
+    mHelpDocs = "Simulates a typical finn in conversation. Format: finnbot"
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+
+    def run(self,line,userObject,destinationObject=None):
+        'Simulates a typical finn in conversation. Format: finnbot'
+        ariQuotes = ["|:", "After hearing you say that, I don't think we can ever be friends", "Brb, cutting down a forest", "Can't answer, I'm shaving and it'll take all day", "Can't hear you over all this atheism!", "Can this wait until after i've listened to this song 100 times on repeat?", "Could use less degrees", "Don't want to hear it, too busy complaining about the tap water", "Goony goon goon", "Hang on, I have to help some micronationalist", "Hey guys, check out my desktop: http://hallo.dr-spangle.com/DESKTOP.PNG", "If we get into a fight, I'll pick you up and run away", "I happen to be an expert on this subject", "I think I've finished constructing a hate engine", "It's about time for me to play through adom again", "It's kind of hard to type while kneeling", "I wish I could answer, but i'm busy redditing", "*lifeless stare*", "Lol, perl", "Lol, remember when i got eli to play crawl for a week?", "Needs moar haskell", "NP: Bad Religion - whatever song", "Remember that thing we were going to do? Now I don't want to do it", "Smells like Oulu", "Some Rahikkala is getting married, you are not invited", "That blows, but I cannot relate to your situation", "This somehow reminds me of my army days", "Whatever, if you'll excuse me, i'm gonna bike 50 kilometers", "You guys are things that say things", "You're under arrest for having too much fun","I have found a new favourite thing to hate"]
+        #ariswearquotes = ["FUCK. FINNISH. PEOPLE!!!", "FUCK MANNERHEIM", "YOU'RE A PERSON OF SHIT"]
+        #if(self.conf['server'][destination[0]]['channel'][destination[1]]['sweardetect']):
+        #    for quote in ariswearquotes:
+        #        ariquotes.append(quote)
+        quote = random.choice(ariQuotes)
+        if(quote[-1] not in ['.','?','!']):
+            quote = quote + '.'
+        return quote
