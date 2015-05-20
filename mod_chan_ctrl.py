482fc47c3d9475d9a1733c945956dcd6b4073274
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-20 22:00:44 +0100

    fn_channel_passive_func() replaced with ChannelPassiveFunctions Function

--- mod_chan_ctrl.py
+++ mod_chan_ctrl.py
@@ -3,21 +3,6 @@ import ircbot_chk
 endl = '\r\n'
 class mod_chan_ctrl:
 
-    def fn_channel_passive_func(self,args,client,destination):
-        'Sets or toggles passive functions for channel, gods only'
-        if(ircbot_chk.ircbot_chk.chk_god(self,destination[0],client)):
-            if(args==''):
-                self.conf['server'][destination[0]]['channel'][destination[1]]['passivefunc'] = not self.conf['server'][destination[0]]['channel'][destination[1]]['passivefunc']
-                return "Passive functions toggled."
-            elif(args.lower()=='true' or args.lower()=='1' or args.lower()=='on'):
-                self.conf['server'][destination[0]]['channel'][destination[1]]['passivefunc'] = True
-                return "Passive functions on."
-            else:
-                self.conf['server'][destination[0]]['channel'][destination[1]]['passivefunc'] = False
-                return "Passive functions off."
-        else:
-            return "Insufficient privileges to set passive functions status."
-
     def fn_channel_pass(self,args,client,destination):
         'Sets a password for a channel, use channel_pass {channel} {password}'
         if(ircbot_chk.ircbot_chk.chk_op(self,destination[0],client)):
