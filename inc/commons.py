bc6694a3ffd63eec2bb3ab708d6dafa722c37a51
joshcoales <joshua@coales.co.uk>
2015-09-09 14:39:40 +0100

    Added Commons.getRandomInt(min,max,count) which will try and get random numbers from random.org API, otherwise return numbers from python's random module

--- inc/commons.py
+++ inc/commons.py
@@ -3,6 +3,7 @@ import datetime
 import urllib.request
 import re
 import json
+import random
 
 class Commons(object):
     '''
@@ -185,4 +186,21 @@ class Commons(object):
                 return True
             return False
         return None
-            
+    
+    @staticmethod
+    def getRandomInt(minInt,maxInt,count=1):
+        'Returns a list of random integers in a given range'
+        #Try and get random numbers from random.org
+        randomOrgUrl = "https://www.random.org/integers/?num="+str(count)+"&format=plain&min="+str(minInt)+"&max="+str(maxInt)+"&col=1&base=10"
+        userAgentHeaders = [["User-Agent","Hallo IRCBot hallo@dr-spangle.com"]]
+        apiResponse = Commons.loadUrlString(randomOrgUrl,userAgentHeaders)
+        if("Error:" not in apiResponse):
+            return apiResponse.split("\n")
+        #Otherwise, use random module
+        outputList = []
+        for _ in range(count):
+            outputList.append(random.randint(minInt,maxInt))
+        return outputList
+
+
+
