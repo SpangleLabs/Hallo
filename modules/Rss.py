03abb8920f5530c059feb1758ec2a9a821cc6f15
jcoales <joshua@infinitybas.com>
2015-12-15 17:01:54 +0000

    Placeholder RssFeedList class

--- modules/Rss.py
+++ modules/Rss.py
@@ -1,10 +1,45 @@
 
 # TODO: RssFeedList class
+class RssFeedList:
+    """
+    Holds the lists of feeds, for loading and unloading.
+    """
+    mFeedList = []
+
+    def addList(self,newList):
+        """
+        Adds a new RSS feed to the list.
+        :param newFeed: RssFeed
+        :return:
+        """
+        self.mFeedList.append(newFeed)
+
+    def getFeedList(self):
+        """
+        Returns the full list of RSS feeds
+        :return: array
+        """
+        return self.mFeedList
+
+    def toXml(self):
+        """
+        Saves the whole feed list to XML file
+        :return:
+        """
+        pass
+
+    @staticmethod
+    def fromXml():
+        """
+        Constructs a new RssFeedList from the XML file
+        :return: RssFeedList
+        """
+        pass
 
 
 class RssFeed:
     """
-    Class representing an rss feed in config
+    Class representing an rss feed in config.
     """
     mTitle = None
     mUrl = None
@@ -22,7 +57,7 @@ class RssFeed:
         # TODO
         pass
 
-    def toXml(self):
+    def toXmlString(self):
         """
         Saves this RssFeed
         :return: string
@@ -30,13 +65,13 @@ class RssFeed:
         pass
 
     @staticmethod
-    def fromXml(xmlString):
+    def fromXmlString(xmlString):
         """
         Loads new RssFeed object from XML string
         :param xmlString: string
         :return: RssFeed
         """
-
+        pass
 
 # TODO: FeedCheck Function class
 # TODO: FeedAdd Function class
