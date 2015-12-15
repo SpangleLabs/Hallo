ad7bef880e8fd52a7ddc2c8d1b6a2258332dd823
jcoales <joshua@infinitybas.com>
2015-12-15 16:23:39 +0000

    Starting placeholder RssFeed class

--- modules/Rss.py
+++ modules/Rss.py
@@ -1,8 +1,44 @@
 
-# TODO:
-# RssFeed class
-# RssFeedList class
-# FeedCheck Function class
-# FeedAdd Function class
-# FeedUpdate Function class
-# FeedRemove Function class
+# TODO: RssFeedList class
+
+
+class RssFeed:
+    """
+    Class representing an rss feed in config
+    """
+    mTitle = None
+    mUrl = None
+    mServerName = None
+    mChannelName = None
+    mUserName = None
+    mLastItem = None
+    mLastCheck = None
+    mUpdateFrequency = None
+
+    def checkFeed(self):
+        """
+        Checks the feed for any updates
+        """
+        # TODO
+        pass
+
+    def toXml(self):
+        """
+        Saves this RssFeed
+        :return: string
+        """
+        pass
+
+    @staticmethod
+    def fromXml(xmlString):
+        """
+        Loads new RssFeed object from XML string
+        :param xmlString: string
+        :return: RssFeed
+        """
+
+
+# TODO: FeedCheck Function class
+# TODO: FeedAdd Function class
+# TODO: FeedUpdate Function class
+# TODO: FeedRemove Function class
