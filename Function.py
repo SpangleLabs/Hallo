7a97c782ba9956afe60b3c69bb1437223774ae6f
joshcoales <joshua@coales.co.uk>
2016-02-08 21:58:36 +0000

    Clearing away PEP8 issues in Function class Also removing unused arguments input from Function.getHelpDocs()

--- Function.py
+++ Function.py
@@ -1,72 +1,82 @@
 
 
 class Function:
-    '''
+    """
     Generic function object. All functions inherit from this.
-    '''
-    mHelpName = None    #Name for use in help listing
-    mNames = set()       #Set of names which can be used to address the function
-    mHelpDocs = None    #Help documentation, if it's just a single line, can be set here
-    #Static constants
-    EVENT_SECOND = "time_second"   #Event which happens every second
-    EVENT_MINUTE = "time_minute"   #Event which happens every minute
-    EVENT_HOUR = "time_hour"       #Event which happens every hour
-    EVENT_DAY = "time_day"         #Event which happens every day
-    EVENT_PING = "ping"            #Event constant signifying a server ping has been received
-    EVENT_MESSAGE = "message"      #Event constant signifying a standard message
-    EVENT_JOIN = "join_channel"    #Event constant signifying someone joined a channel
-    EVENT_LEAVE = "leave_channel"  #Event constant signifying someone left a channel
-    EVENT_QUIT = "quit"            #Event constant signifying someone disconnected
-    EVENT_CHNAME = "name_change"   #Event constant signifying someone changed their name
-    EVENT_KICK = "kick"            #Event constant signifying someone was forcibly removed from the channel
-    EVENT_INVITE = "invite"        #Event constant signifying someone has invited hallo to a channel
-    EVENT_NOTICE = "notice"        #Event constant signifying a notice was received. (IRC only?)
-    EVENT_MODE = "mode_change"     #Event constant signifying a channel mode change. (IRC only?)
-    EVENT_CTCP = "message_ctcp"    #Event constant signifying a CTCP message received (IRC only)
-    EVENT_NUMERIC = "numeric"      #Event constant signifying a numeric message from a server (IRC only)
-    EVENT_RAW = "raw"              #Event constant signifying raw data received from server which doesn't fit the above
+    """
+    mHelpName = None    # Name for use in help listing
+    mNames = set()      # Set of names which can be used to address the function
+    mHelpDocs = None    # Help documentation, if it's just a single line, can be set here
+    # Static constants
+    EVENT_SECOND = "time_second"   # Event which happens every second
+    EVENT_MINUTE = "time_minute"   # Event which happens every minute
+    EVENT_HOUR = "time_hour"       # Event which happens every hour
+    EVENT_DAY = "time_day"         # Event which happens every day
+    EVENT_PING = "ping"            # Event constant signifying a server ping has been received
+    EVENT_MESSAGE = "message"      # Event constant signifying a standard message
+    EVENT_JOIN = "join_channel"    # Event constant signifying someone joined a channel
+    EVENT_LEAVE = "leave_channel"  # Event constant signifying someone left a channel
+    EVENT_QUIT = "quit"            # Event constant signifying someone disconnected
+    EVENT_CHNAME = "name_change"   # Event constant signifying someone changed their name
+    EVENT_KICK = "kick"            # Event constant signifying someone was forcibly removed from the channel
+    EVENT_INVITE = "invite"        # Event constant signifying someone has invited hallo to a channel
+    EVENT_NOTICE = "notice"        # Event constant signifying a notice was received. (IRC only?)
+    EVENT_MODE = "mode_change"     # Event constant signifying a channel mode change. (IRC only?)
+    EVENT_CTCP = "message_ctcp"    # Event constant signifying a CTCP message received (IRC only)
+    EVENT_NUMERIC = "numeric"      # Event constant signifying a numeric message from a server (IRC only)
+    EVENT_RAW = "raw"              # Event constant signifying raw data received from server which doesn't fit the above
     
-    def run(self,line,userObject,destinationObject):
-        'Runs the function when it is called directly'
+    def run(self, line, userObject, destinationObject):
+        """Runs the function when it is called directly
+        :param line: User supplied arguments for this function call
+        :param userObject: User who called the function
+        :param destinationObject: Destination the function was called from, equal to user if private message
+        """
         raise NotImplementedError
 
     @staticmethod
     def isPersistent():
-        'Returns boolean representing whether this function is supposed to be persistent or not'
+        """Returns boolean representing whether this function is supposed to be persistent or not"""
         return False
     
     @staticmethod
     def loadFunction():
-        'Loads the function, persistent functions only.'
+        """Loads the function, persistent functions only."""
         return Function()
     
     def saveFunction(self):
-        'Saves the function, persistent functions only.'
+        """Saves the function, persistent functions only."""
         return None
     
     def getPassiveEvents(self):
-        'Returns a list of events which this function may want to respond to in a passive way'
+        """Returns a list of events which this function may want to respond to in a passive way"""
         return set()
 
-    def passiveRun(self,event,fullLine,serverObject,userObject=None,channelObject=None):
-        'Replies to an event not directly addressed to the bot.'
+    def passiveRun(self, event, fullLine, serverObject, userObject=None, channelObject=None):
+        """Replies to an event not directly addressed to the bot.
+        :param event: Event which has called the function
+        :param fullLine: Full user input line which came with the event
+        :param serverObject: Server object which fired the event, or none if server independent
+        :param userObject: User which triggered the event, or none if not user triggered
+        :param channelObject: Channel the event was triggered on, or none if not triggered on channel
+        """
         pass
         
     def getHelpName(self):
-        'Returns the name to be printed for help documentation'
-        if(self.mHelpName is None):
+        """Returns the name to be printed for help documentation"""
+        if self.mHelpName is None:
             raise NotImplementedError
         return self.mHelpName
     
-    def getHelpDocs(self,arguments=None):
-        'Returns the help documentation, specific to given arguments, if supplied'
-        if(self.mHelpDocs is None):
+    def getHelpDocs(self):
+        """
+        Returns the help documentation, specific to given arguments, if supplied
+        """
+        if self.mHelpDocs is None:
             raise NotImplementedError
         return self.mHelpDocs
     
     def getNames(self):
-        'Returns the list of names for directly addressing the function'
+        """Returns the list of names for directly addressing the function"""
         self.mNames.add(self.mHelpName)
         return self.mNames
-    
-    
