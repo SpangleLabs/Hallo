23a5df625aca97798c77f0eeecc5cde868af88a0
joshcoales <joshua@coales.co.uk>
2016-04-04 09:38:28 +0100

    Converting Printer class for PEP8 compliance

--- inc/Printer.py
+++ inc/Printer.py
@@ -2,18 +2,19 @@ from Function import Function
 from inc.commons import Commons
 import datetime
 
+
 class Printer:
-    '''
+    """
     Printing class. This is created and stored by the Hallo object.
-    It exists in order to provie a single entry point to all printing to screen.
-    '''
+    It exists in order to provide a single entry point to all printing to screen.
+    """
     mHallo = None
     mEventDict = None
 
-    def __init__(self,hallo):
-        '''
+    def __init__(self, hallo):
+        """
         Constructor
-        '''
+        """
         self.mHallo = hallo
         self.mEventDict = {}
         self.mEventDict[Function.EVENT_SECOND] = self.printSecond
@@ -32,129 +33,129 @@ class Printer:
         self.mEventDict[Function.EVENT_MODE] = self.printModeChange
         self.mEventDict[Function.EVENT_CTCP] = self.printCtcp
     
-    def output(self,event,fullLine,serverObject=None,userObject=None,channelObject=None):
-        'The function which actually prints the messages.'
-        #If channel or server are set to all, set to None for getting output
-        if(serverObject==Commons.ALL_SERVERS):
+    def output(self, event, fullLine, serverObject=None, userObject=None, channelObject=None):
+        """The function which actually prints the messages."""
+        # If channel or server are set to all, set to None for getting output
+        if serverObject == Commons.ALL_SERVERS:
             serverObject = None
-        if(channelObject==Commons.ALL_CHANNELS):
+        if channelObject == Commons.ALL_CHANNELS:
             channelObject = None
-        #Check what type of event and pass to that to create line
-        if(event not in self.mEventDict):
+        # Check what type of event and pass to that to create line
+        if event not in self.mEventDict:
             return None
         printFunction = self.mEventDict[event]
-        printLine = printFunction(fullLine,serverObject,userObject,channelObject)
-        #Output the log line
+        printLine = printFunction(fullLine, serverObject, userObject, channelObject)
+        # Output the log line
         print(printLine)
         return None
     
-    def outputFromSelf(self,event,fullLine,serverObject=None,userObject=None,channelObject=None):
-        'Prints lines for messages from hallo.'
-        #Check what type of event and pass to that to create line
-        if(event not in self.mEventDict):
+    def outputFromSelf(self, event, fullLine, serverObject=None, userObject=None, channelObject=None):
+        """Prints lines for messages from hallo."""
+        # Check what type of event and pass to that to create line
+        if event not in self.mEventDict:
             return None
         printFunction = self.mEventDict[event]
         halloUserObject = serverObject.getUserByName(serverObject.getNick())
-        printLine = printFunction(fullLine,serverObject,halloUserObject,channelObject)
-        #Write the log line
+        printLine = printFunction(fullLine, serverObject, halloUserObject, channelObject)
+        # Write the log line
         print(printLine)
         return None
     
-    def printSecond(self,fullLine,serverObject,userObject,channelObject):
+    def printSecond(self, fullLine, serverObject, userObject, channelObject):
         return None
     
-    def printMinute(self,fullLine,serverObject,userObject,channelObject):
+    def printMinute(self, fullLine, serverObject, userObject, channelObject):
         return None
     
-    def printHour(self,fullLine,serverObject,userObject,channelObject):
+    def printHour(self, fullLine, serverObject, userObject, channelObject):
         return None
     
-    def printDay(self,fullLine,serverObject,userObject,channelObject):
+    def printDay(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "Day changed: "+datetime.datetime.now().strftime("%Y-%m-%d")
         return output
     
-    def printPing(self,fullLine,serverObject,userObject,channelObject):
+    def printPing(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
-        if(userObject is None):
+        if userObject is None:
             output += "["+serverObject.getName()+"] PING"
         else:
             output += "["+serverObject.getName()+"] PONG"
         return output
     
-    def printMessage(self,fullLine,serverObject,userObject,channelObject):
+    def printMessage(self, fullLine, serverObject, userObject, channelObject):
         destinationObject = channelObject
-        if(channelObject==None):
+        if channelObject is None:
             destinationObject = userObject
         output = Commons.currentTimestamp() + " "
-        output += "[" +serverObject.getName()+ "] "
+        output += "[" + serverObject.getName() + "] "
         output += destinationObject.getName() + " "
         output += "<" + userObject.getName() + "> " + fullLine
         return output
     
-    def printJoin(self,fullLine,serverObject,userObject,channelObject):
+    def printJoin(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "[" + serverObject.getName() + "] "
         output += userObject.getName() + " joined " + channelObject.getName()
         return output
     
-    def printLeave(self,fullLine,serverObject,userObject,channelObject):
+    def printLeave(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "[" + serverObject.getName() + "] "
         output += userObject.getName() + " left " + channelObject.getName()
-        if(fullLine.strip()!=""):
+        if fullLine.strip() != "":
             output += " (" + fullLine + ")"
         return output
     
-    def printQuit(self,fullLine,serverObject,userObject,channelObject):
+    def printQuit(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "[" + serverObject.getName() + "] "
         output += userObject.getName() + " has quit."
-        if(fullLine.strip()!=""):
+        if fullLine.strip() != "":
             output += " (" + fullLine + ")"
         return output
     
-    def printNameChange(self,fullLine,serverObject,userObject,channelObject):
+    def printNameChange(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "[" + serverObject.getName() + "] "
         output += "Nick change: " + fullLine + " -> " + userObject.getName()
         return output
     
-    def printKick(self,fullLine,serverObject,userObject,channelObject):
+    def printKick(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "[" + serverObject.getName() + "] "
         output += userObject.getName() + " was kicked from " + channelObject.getName()
-        if(fullLine.strip()!=""):
+        if fullLine.strip() != "":
             output += " (" + fullLine + ")"
         return output
     
-    def printInvite(self,fullLine,serverObject,userObject,channelObject):
+    def printInvite(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "[" + serverObject.getName() + "] "
         output += "Invite to " + channelObject.getName() + ' from ' + userObject.getName()
         return output
     
-    def printNotice(self,fullLine,serverObject,userObject,channelObject):
+    def printNotice(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "[" + serverObject.getName() + "] "
         output += "Notice from " + userObject.getName() + ": " + fullLine
         return output
     
-    def printModeChange(self,fullLine,serverObject,userObject,channelObject):
+    def printModeChange(self, fullLine, serverObject, userObject, channelObject):
         output = Commons.currentTimestamp() + " "
         output += "[" + serverObject.getName() + "] "
         output += userObject.getName() + ' set ' + fullLine + ' on ' + channelObject.getName()
         return output
     
-    def printCtcp(self,fullLine,serverObject,userObject,channelObject):
-        #Get useful data and objects
+    def printCtcp(self, fullLine, serverObject, userObject, channelObject):
+        # Get useful data and objects
         ctcpCommand = fullLine.split()[0]
         ctcpArguments = ' '.join(fullLine.split()[1:])
         destinationObject = channelObject
-        if(channelObject==None):
+        if channelObject is None:
             destinationObject = userObject
-        #Print CTCP actions differently to other CTCP commands
-        if(ctcpCommand.lower()=="action"):
+        # Print CTCP actions differently to other CTCP commands
+        if ctcpCommand.lower() == "action":
             output = Commons.currentTimestamp() + " "
             output += "[" + serverObject.getName() + "] "
             output += destinationObject.getName() + " "
@@ -165,4 +166,3 @@ class Printer:
         output += destinationObject.getName() + " "
         output += "<" + userObject.getName() + " (CTCP)> " + fullLine
         return output
-
