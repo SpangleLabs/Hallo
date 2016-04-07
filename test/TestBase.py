513b3a1a0f98bca1fab990d50966f7305c77930b
joshcoales <joshua@coales.co.uk>
2016-04-07 09:12:59 +0100

    Reorganising tests

---
+++ test/TestBase.py
@@ -0,0 +1,28 @@
+from threading import Thread
+
+from FunctionDispatcher import FunctionDispatcher
+from Hallo import Hallo
+import unittest
+from test.ServerMock import ServerMock
+
+
+class TestBase(unittest.TestCase):
+
+    def setUp(self):
+        # Create a Hallo
+        self.hallo = Hallo()
+        # only 1 module, only 1 (mock) server
+        self.function_dispatcher = FunctionDispatcher({"Math"}, self.hallo)
+        self.hallo.function_dispatcher = self.function_dispatcher
+        self.server = ServerMock(self.hallo)
+        # self.server = unittest.mock.Mock()
+        self.hallo.server_list = [self.server]
+        # send shit in, check shit out
+        self.hallo_thread = Thread(target=self.hallo.start,)
+        self.hallo_thread.start()
+        self.test_user = self.server.get_user_by_name("test")
+        self.server.get_send_data()
+
+    def tearDown(self):
+        self.hallo.open = False
+        self.hallo_thread.join()
---
+++ test/modules/MathTest.py
@@ -0,0 +1,20 @@
+from Server import Server
+from test.TestBase import TestBase
+
+
+class MathTest(TestBase):
+
+    def test_calc_simple(self):
+        self.function_dispatcher.dispatch("calc 2+2", self.test_user, self.test_user)
+        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        assert data[0][0] == "4"
+
+    def test_calc_multiply(self):
+        self.function_dispatcher.dispatch("calc 21*56", self.test_user, self.test_user)
+        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        assert data[0][0] == "1176"
+
+    def test_calc_divide(self):
+        self.function_dispatcher.dispatch("calc 1/5", self.test_user, self.test_user)
+        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        assert data[0][0] == "0.2"
