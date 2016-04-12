b466c1c038a97e92c35ab757c5ba340833145222
joshcoales <joshua@coales.co.uk>
2016-04-12 22:29:27 +0100

    Adding Trump function test suite

---
+++ test/modules/SillyEtd/testTrump.py
@@ -0,0 +1,31 @@
+import unittest
+
+from Server import Server
+from test.TestBase import TestBase
+
+
+class TrumpTest(TestBase, unittest.TestCase):
+
+    def test_trump_simple(self):
+        self.function_dispatcher.dispatch("trump", self.test_user, self.test_user)
+        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        assert "error" not in data[0][0].lower(), "Trump function is returning error."
+        assert "IMPERATOR TRUMP!" in data[0][0], "Trump function does not declare imperator trump."
+
+    def test_trump_num(self):
+        self.function_dispatcher.dispatch("trump 7", self.test_user, self.test_user)
+        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        print(data[0][0])
+        assert data[0][0].count("Trump") == 7, "Trump numerical input not working."
+
+    def test_trump_max(self):
+        self.function_dispatcher.dispatch("trump 10", self.test_user, self.test_user)
+        data10 = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        self.function_dispatcher.dispatch("trump 20", self.test_user, self.test_user)
+        data20 = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        assert data10[0][0] == data20[0][0], "Trump function max limit is not working."
+
+    def test_trump_str(self):
+        self.function_dispatcher.dispatch("trump yeah!", self.test_user, self.test_user)
+        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        assert "error" not in data[0][0].lower(), "Trump function is not working when given invalid number."
