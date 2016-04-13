cfe8bb3a024d27797bc0fba209383f57c911bd11
joshcoales <joshua@coales.co.uk>
2016-04-13 22:33:44 +0100

    Fixing test names in CorbynTest class

--- test/modules/SillyEtd/testCorbyn.py
+++ test/modules/SillyEtd/testCorbyn.py
@@ -6,25 +6,25 @@ from test.TestBase import TestBase
 
 class CorbynTest(TestBase, unittest.TestCase):
 
-    def test_trump_simple(self):
+    def test_corbyn_simple(self):
         self.function_dispatcher.dispatch("corbyn", self.test_user, self.test_user)
         data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
         assert "error" not in data[0][0].lower(), "Corbyn function is returning error."
         assert "CHAIRMAN CORBYN!" in data[0][0], "Corbyn function does not declare imperator trump."
 
-    def test_trump_num(self):
+    def test_corbyn_num(self):
         self.function_dispatcher.dispatch("corbyn 7", self.test_user, self.test_user)
         data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
         assert data[0][0].count("Corbyn") == 7, "Corbyn numerical input not working."
 
-    def test_trump_max(self):
+    def test_corbyn_max(self):
         self.function_dispatcher.dispatch("corbyn 10", self.test_user, self.test_user)
         data10 = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
         self.function_dispatcher.dispatch("corbyn 20", self.test_user, self.test_user)
         data20 = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
         assert data10[0][0] == data20[0][0], "Corbyn function max limit is not working."
 
-    def test_trump_str(self):
+    def test_corbyn_str(self):
         self.function_dispatcher.dispatch("corbyn woo!", self.test_user, self.test_user)
         data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
         assert "error" not in data[0][0].lower(), "Corbyn function is not working when given invalid number."
