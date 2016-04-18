870bd2ce58a4b0c6198048c4330e4a1f891aa2de
joshcoales <joshua@coales.co.uk>
2016-04-18 15:40:25 +0100

    Adding AsciiArt module to test framework

--- test/TestBase.py
+++ test/TestBase.py
@@ -13,7 +13,7 @@ class TestBase(unittest.TestCase):
         # Create a Hallo
         self.hallo = Hallo()
         # only 1 module, only 1 (mock) server
-        self.function_dispatcher = FunctionDispatcher({"Euler", "Math", "SillyEtd"}, self.hallo)
+        self.function_dispatcher = FunctionDispatcher({"AsciiArt", "Euler", "Math", "SillyEtd"}, self.hallo)
         self.hallo.function_dispatcher = self.function_dispatcher
         self.server = ServerMock(self.hallo)
         # self.server = unittest.mock.Mock()
