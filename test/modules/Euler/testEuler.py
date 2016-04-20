97b9bddb6df2830b5fb4601c0dc88cc8cb49efb5
joshcoales <joshua@coales.co.uk>
2016-04-20 19:20:14 +0100

    Adding test of project euler solutions

--- test/modules/Euler/testEuler.py
+++ test/modules/Euler/testEuler.py
@@ -14,24 +14,25 @@ class EulerTest(TestBase, unittest.TestCase):
         data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
         assert "error" not in data[0][0].lower(), "Euler function should not throw errors."
 
-    def test_euler_1(self):
-        prob_num = "1"
-        prob_ans = "233168"
-        self.function_dispatcher.dispatch("euler "+prob_num, self.test_user, self.test_user)
-        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
-        assert "error" not in data[0][0].lower(), "Euler problem "+prob_num+" throws an error."
-        assert "Euler project problem "+prob_num+"?" in data[0][0], "Problem name is not in output for " \
-                                                                    "problem "+prob_num
-        assert prob_ans in data[0][0].lower(), "Euler problem "+prob_num+" has incorrect answer. It should return " + \
-                                               prob_ans+" but it returns: "+data[0][0]
-
-    def test_euler_2(self):
-        prob_num = "2"
-        prob_ans = "4613732"
-        self.function_dispatcher.dispatch("euler "+prob_num, self.test_user, self.test_user)
-        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
-        assert "error" not in data[0][0].lower(), "Euler problem "+prob_num+" throws an error."
-        assert "Euler project problem "+prob_num+"?" in data[0][0], "Problem name is not in output for " \
-                                                                    "problem "+prob_num
-        assert prob_ans in data[0][0].lower(), "Euler problem "+prob_num+" has incorrect answer. It should return " + \
-                                               prob_ans+" but it returns: "+data[0][0]
+    def test_euler_solutions(self):
+        # Spoilers
+        prob_dict = {"1": "233168", "2": "4613732", "3": "6857", "4": "906609", "5": "232792560", "6": "25164150",
+                     "7": "104743", "8": "23514624000", "9": "31875000", "10": "142913828922", "11": "70600674",
+                     "12": "76576500", "13": "5537376230", "14": "837799", "15": "137846528820", "16": "1366",
+                     "17": "21124", "18": "1074", "19": "171", "20": "648", "21": "31626", "22": "871198282",
+                     "23": "4179871", "24": "2783915460", "25": "4782", "26": "983", "27": "-59231", "28": "669171001",
+                     "29": "9183", "30": "443839", "31": "73682", "32": "45228", "33": "100", "34": "40730", "35": "55",
+                     "36": "872187", "37": "748317", "38": "932718654", "39": "840", "40": "210", "41": "7652413",
+                     "42": "162", "43": "16695334890", "44": "5482660", "45": "1533776805", "46": "5777",
+                     "47": "134043", "48": "9110846700", "49": "296962999629", "50": "997651", "51": "121313",
+                     "52": "142857", "53": "4075", "54": "376", "55": "249", "56": "972", "57": "153", "58": "26241",
+                     "59": "107359", "60": "26033", "67": "7273"}
+        for prob_num, prob_ans in prob_dict.items():
+            self.function_dispatcher.dispatch("euler " + prob_num, self.test_user, self.test_user)
+            data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+            assert "error" not in data[0][0].lower(), "Euler problem " + prob_num + " throws an error."
+            assert "Euler project problem " + prob_num + "?" in data[0][0], "Problem name is not in output for " \
+                                                                            "problem " + prob_num
+            assert prob_ans in data[0][0].lower(), "Euler problem " + prob_num + \
+                                                   " has incorrect answer. It should return " + prob_ans + \
+                                                   " but it returns: " + data[0][0]
