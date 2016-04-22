aa5f34c2d7d8f4f25d65158fbfa7f56ed64a9ec0
joshcoales <joshua@coales.co.uk>
2016-04-22 09:24:34 +0100

    Flipping assert statements in testAlarm

--- test/modules/Silly/testAlarm.py
+++ test/modules/Silly/testAlarm.py
@@ -9,10 +9,10 @@ class AlarmTest(TestBase, unittest.TestCase):
     def test_alarm_simple(self):
         self.function_dispatcher.dispatch("alarm", self.test_user, self.test_user)
         data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
-        assert "wooo" not in data[0][0].lower(), "Alarm function not going woo."
+        assert "wooo" in data[0][0].lower(), "Alarm function not going woo."
 
     def test_alarm_word(self):
         self.function_dispatcher.dispatch("alarm nerd", self.test_user, self.test_user)
         data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
-        assert "wooo" not in data[0][0].lower(), "Alarm function not going woo."
-        assert "nerd" not in data[0][0].lower(), "Alarm function not going responding with word input."
+        assert "wooo" in data[0][0].lower(), "Alarm function not going woo."
+        assert "nerd" in data[0][0].lower(), "Alarm function not going responding with word input."
