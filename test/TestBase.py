afa4d6a299b11998956125d1cc67ea1094ece53b
joshcoales <joshua@coales.co.uk>
2016-04-28 15:23:05 +0100

    Ensuring mock server in test base has a name

--- test/TestBase.py
+++ test/TestBase.py
@@ -17,6 +17,7 @@ class TestBase(unittest.TestCase):
                                                       self.hallo)
         self.hallo.function_dispatcher = self.function_dispatcher
         self.server = ServerMock(self.hallo)
+        self.server.name = "mock-server"
         # self.server = unittest.mock.Mock()
         self.hallo.server_list = [self.server]
         # send shit in, check shit out
