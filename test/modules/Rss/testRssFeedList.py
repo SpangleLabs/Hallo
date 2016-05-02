031df364b37f9aaae592df5ded8b32db7d89ffe4
joshcoales <joshua@coales.co.uk>
2016-05-02 21:03:07 +0100

    Test for RssFeedList.add_feed()

--- test/modules/Rss/testRssFeedList.py
+++ test/modules/Rss/testRssFeedList.py
@@ -1,5 +1,7 @@
 import unittest
-from modules.Rss import RssFeedList
+
+from inc.Commons import Commons
+from modules.Rss import RssFeedList, RssFeed
 
 
 class TestRssFeedList(unittest.TestCase):
@@ -7,3 +9,14 @@ class TestRssFeedList(unittest.TestCase):
     def test_init(self):
         rfl = RssFeedList()
         assert rfl.feed_list == []
+
+    def test_add_feed(self):
+        rfl = RssFeedList()
+        assert rfl.feed_list == []
+        # Create example rss feed
+        rf = RssFeed()
+        rf.update_frequency = Commons.load_time_delta("P0T3600S")
+        rf.url = "http://spangle.org.uk/hallo/test_feed.xml"
+        rfl.add_feed(rf)
+        assert len(rfl.feed_list) == 1
+        assert rfl.feed_list[0] == rf
