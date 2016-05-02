e6facd07162f10262e88e58cd1cb3919d72a9d69
joshcoales <joshua@coales.co.uk>
2016-05-02 21:08:53 +0100

    Test for RssFeedList.remove_feed()

--- test/modules/Rss/testRssFeedList.py
+++ test/modules/Rss/testRssFeedList.py
@@ -20,3 +20,18 @@ class TestRssFeedList(unittest.TestCase):
         rfl.add_feed(rf)
         assert len(rfl.feed_list) == 1
         assert rfl.feed_list[0] == rf
+
+    def test_remove_feed(self):
+        # Setup a feed list
+        rfl = RssFeedList()
+        rf1 = RssFeed()
+        rf1.url = "http://spangle.org.uk/hallo/test_feed.xml?1"
+        rfl.add_feed(rf1)
+        rf2 = RssFeed()
+        rf2.url = "http://spangle.org.uk/hallo/test_feed.xml?2"
+        rfl.add_feed(rf2)
+        assert len(rfl.feed_list) == 2
+        # Remove an item from the feed list
+        rfl.remove_feed(rf1)
+        assert len(rfl.feed_list) == 1
+        assert rfl.feed_list[0] == rf2
