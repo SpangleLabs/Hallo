15c27f6d4bf3a14f93f0ffe1e459221aa76d0c9d
joshcoales <joshua@coales.co.uk>
2016-05-02 21:57:14 +0100

    Test for RssFeedList.get_feeds_by_url()

--- test/modules/Rss/testRssFeedList.py
+++ test/modules/Rss/testRssFeedList.py
@@ -111,6 +111,52 @@ class TestRssFeedList(unittest.TestCase):
         assert rf3 in feed_list
         assert rf5 in feed_list
 
+    def test_get_feeds_by_url(self):
+        serv1 = ServerMock(None)
+        serv1.name = "test_serv1"
+        serv2 = ServerMock(None)
+        serv2.name = "test_serv2"
+        chan1 = serv1.get_channel_by_name("test_chan1")
+        user2 = serv1.get_user_by_name("test_user2")
+        chan3 = serv2.get_channel_by_name("test_chan3")
+        # Setup a feed list
+        rfl = RssFeedList()
+        rf1 = RssFeed()
+        rf1.url = "http://spangle.org.uk/hallo/test_feed.xml?1"
+        rf1.title = "test_feed1"
+        rf1.server_name = chan1.server.name
+        rf1.channel_name = chan1.name
+        rfl.add_feed(rf1)
+        rf2 = RssFeed()
+        rf2.url = "http://spangle.org.uk/hallo/test_feed.xml?2"
+        rf2.title = "test_feed2"
+        rf2.server_name = user2.server.name
+        rf2.user_name = user2.name
+        rfl.add_feed(rf2)
+        rf3 = RssFeed()
+        rf3.url = "http://spangle.org.uk/hallo/test_feed.xml?3"
+        rf3.title = "test_feed3"
+        rf3.server_name = chan3.server.name
+        rf3.channel_name = chan3.name
+        rfl.add_feed(rf3)
+        rf4 = RssFeed()
+        rf4.url = "http://spangle.org.uk/hallo/test_feed.xml?4"
+        rf4.title = "test_feed4"
+        rf4.server_name = chan3.server.name
+        rf4.channel_name = chan3.name
+        rfl.add_feed(rf4)
+        rf5 = RssFeed()
+        rf5.url = "http://spangle.org.uk/hallo/test_feed.xml?4"
+        rf5.title = "test_feed3"
+        rf5.server_name = chan3.server.name
+        rf5.channel_name = chan3.name
+        rfl.add_feed(rf5)
+        # Check function
+        feed_list = rfl.get_feeds_by_url("http://spangle.org.uk/hallo/test_feed.xml?4", chan3)
+        assert len(feed_list) == 2
+        assert rf4 in feed_list
+        assert rf5 in feed_list
+
     def test_remove_feed(self):
         # Setup a feed list
         rfl = RssFeedList()
