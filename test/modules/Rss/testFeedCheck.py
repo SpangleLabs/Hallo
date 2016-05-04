46301ac9e663033452f9c9e96a2ce7ccc687dbaa
joshcoales <joshua@coales.co.uk>
2016-05-04 11:36:01 +0100

    Test for FeedCheck.save_function()

--- test/modules/Rss/testFeedCheck.py
+++ test/modules/Rss/testFeedCheck.py
@@ -11,3 +11,19 @@ class FeedCheckTest(TestBase, unittest.TestCase):
         fc = FeedCheck()
         assert fc.rss_feed_list is not None
         assert fc.rss_feed_list.feed_list == []
+
+    def test_save_function(self):
+        fc = FeedCheck()
+        # Mock out the rss feed list
+        mfl = MockRssFeedList()
+        fc.rss_feed_list = mfl
+        assert mfl.to_xml_called
+
+
+class MockRssFeedList:
+
+    def __init__(self):
+        self.to_xml_called = False
+
+    def to_xml(self):
+        self.to_xml_called = True
