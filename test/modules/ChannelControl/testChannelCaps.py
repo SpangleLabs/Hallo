837d1a4002eb36d6bad27561be9ccb7cc2ef8754
joshcoales <joshua@coales.co.uk>
2016-05-08 21:58:25 +0100

    Filling in ChannelCapsTest.test_caps_toggle()

--- test/modules/ChannelControl/testChannelCaps.py
+++ test/modules/ChannelControl/testChannelCaps.py
@@ -1,12 +1,24 @@
 import unittest
 
+from Server import Server
 from test.TestBase import TestBase
 
 
 class ChannelCapsTest(TestBase, unittest.TestCase):
 
     def test_caps_toggle(self):
-        pass
+        self.test_chan.use_caps_lock = False
+        self.function_dispatcher.dispatch("channel caps", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" not in data[0][0].lower()
+        assert "toggle" in data[0][0].lower()
+        assert self.test_chan.use_caps_lock
+        # Try toggling again
+        self.function_dispatcher.dispatch("channel caps", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" not in data[0][0].lower()
+        assert "toggle" in data[0][0].lower()
+        assert not self.test_chan.use_caps_lock
 
     def test_caps_on(self):
         pass
