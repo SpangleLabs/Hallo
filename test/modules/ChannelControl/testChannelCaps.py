c223e79d5c9d4be45c01755a9d4f85ddc63f0a8d
joshcoales <joshua@coales.co.uk>
2016-05-08 23:48:34 +0100

    Filling in ChannelCapsTest.test_caps_channel_on() and test_caps_channel_off()

--- test/modules/ChannelControl/testChannelCaps.py
+++ test/modules/ChannelControl/testChannelCaps.py
@@ -53,13 +53,33 @@ class ChannelCapsTest(TestBase, unittest.TestCase):
         assert not test_chan1.use_caps_lock
 
     def test_caps_channel_on(self):
-        pass
+        test_chan1 = self.server.get_channel_by_name("other_channel")
+        test_chan1.in_channel = True
+        test_chan1.use_caps_lock = False
+        self.function_dispatcher.dispatch("channel caps other_channel on", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" not in data[0][0].lower()
+        assert "caps lock set on" in data[0][0].lower()
+        assert self.test_chan.use_caps_lock
 
     def test_caps_channel_off(self):
-        pass
+        test_chan1 = self.server.get_channel_by_name("other_channel")
+        test_chan1.in_channel = True
+        test_chan1.use_caps_lock = True
+        self.function_dispatcher.dispatch("channel caps other_channel off", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" not in data[0][0].lower()
+        assert "caps lock set off" in data[0][0].lower()
+        assert not self.test_chan.use_caps_lock
 
     def test_caps_on_channel(self):
         pass
 
     def test_caps_off_channel(self):
+        pass
+
+    def test_caps_not_in_channel_toggle(self):
+        pass
+
+    def test_caps_not_in_channel_on(self):
         pass
