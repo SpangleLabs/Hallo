ca6489e838b78547e9d98b75bf8fcb8d3630616b
joshcoales <joshua@coales.co.uk>
2016-05-08 21:52:12 +0100

    Starting test class for testing ChannelCaps function

---
+++ test/modules/ChannelControl/testChannelCaps.py
@@ -0,0 +1,24 @@
+import unittest
+
+from test.TestBase import TestBase
+
+
+class ChannelCapsTest(TestBase, unittest.TestCase):
+
+    def test_caps_toggle(self):
+        pass
+
+    def test_caps_on(self):
+        pass
+
+    def test_caps_off(self):
+        pass
+
+    def test_caps_channel_toggle(self):
+        pass
+
+    def test_caps_channel_bool(self):
+        pass
+
+    def test_caps_bool_channel(self):
+        pass
