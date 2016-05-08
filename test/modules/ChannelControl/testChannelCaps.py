c3f4d028ed77abed573ac4b8df0997926cb5eb15
joshcoales <joshua@coales.co.uk>
2016-05-08 23:54:41 +0100

    Filling in all remaining tests for ChannelCapsTest

--- test/modules/ChannelControl/testChannelCaps.py
+++ test/modules/ChannelControl/testChannelCaps.py
@@ -73,13 +73,48 @@ class ChannelCapsTest(TestBase, unittest.TestCase):
         assert not self.test_chan.use_caps_lock
 
     def test_caps_on_channel(self):
-        pass
+        test_chan1 = self.server.get_channel_by_name("other_channel")
+        test_chan1.in_channel = True
+        test_chan1.use_caps_lock = False
+        self.function_dispatcher.dispatch("channel caps on other_channel", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" not in data[0][0].lower()
+        assert "caps lock set on" in data[0][0].lower()
+        assert self.test_chan.use_caps_lock
 
     def test_caps_off_channel(self):
-        pass
+        test_chan1 = self.server.get_channel_by_name("other_channel")
+        test_chan1.in_channel = True
+        test_chan1.use_caps_lock = True
+        self.function_dispatcher.dispatch("channel caps off other_channel", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" not in data[0][0].lower()
+        assert "caps lock set off" in data[0][0].lower()
+        assert not self.test_chan.use_caps_lock
 
     def test_caps_not_in_channel_toggle(self):
-        pass
+        test_chan1 = self.server.get_channel_by_name("other_channel")
+        test_chan1.in_channel = False
+        test_chan1.use_caps_lock = False
+        self.function_dispatcher.dispatch("channel caps other_channel", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" in data[0][0].lower()
+        assert not test_chan1.use_caps_lock
 
     def test_caps_not_in_channel_on(self):
-        pass
+        test_chan1 = self.server.get_channel_by_name("other_channel")
+        test_chan1.in_channel = False
+        test_chan1.use_caps_lock = False
+        self.function_dispatcher.dispatch("channel caps other_channel on", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" in data[0][0].lower()
+        assert not test_chan1.use_caps_lock
+
+    def test_caps_no_bool(self):
+        test_chan1 = self.server.get_channel_by_name("other_channel")
+        test_chan1.in_channel = False
+        test_chan1.use_caps_lock = False
+        self.function_dispatcher.dispatch("channel caps other_channel word", self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        assert "error" in data[0][0].lower()
+        assert not test_chan1.use_caps_lock
