d110eb9b427c1af858c67ea6deae1d3584db42fd
joshcoales <joshua@coales.co.uk>
2016-05-12 11:05:34 +0100

    Implementing ChannelCapsTest.test_op_not_irc()

--- test/modules/ChannelControl/testOperator.py
+++ test/modules/ChannelControl/testOperator.py
@@ -1,12 +1,24 @@
 import unittest
 
 from Server import Server
+from test.ServerMock import ServerMock
 from test.TestBase import TestBase
 
 
 class ChannelCapsTest(TestBase, unittest.TestCase):
 
     def test_op_not_irc(self):
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_name("test_chan1")
+        user1 = serv1.get_user_by_name("test_user1")
+        try:
+            self.function_dispatcher.dispatch("op", user1, chan1)
+            data = serv1.get_send_data(1, chan1, Server.MSG_MSG)
+            assert "error" in data[0][0].lower()
+        finally:
+            self.hallo.remove_server(serv1)
         pass
 
     def test_op_0_privmsg(self):
