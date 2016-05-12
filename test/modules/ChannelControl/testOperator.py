d1ac4aaaf12a6ef29a62839eb1be1630c3ec54c2
joshcoales <joshua@coales.co.uk>
2016-05-12 12:10:39 +0100

    Implementing OperatorTest.test_op_0_no_power()

--- test/modules/ChannelControl/testOperator.py
+++ test/modules/ChannelControl/testOperator.py
@@ -5,7 +5,7 @@ from test.ServerMock import ServerMock
 from test.TestBase import TestBase
 
 
-class ChannelCapsTest(TestBase, unittest.TestCase):
+class OperatorTest(TestBase, unittest.TestCase):
 
     def test_op_not_irc(self):
         serv1 = ServerMock(self.hallo)
@@ -14,10 +14,11 @@ class ChannelCapsTest(TestBase, unittest.TestCase):
         self.hallo.add_server(serv1)
         chan1 = serv1.get_channel_by_name("test_chan1")
         user1 = serv1.get_user_by_name("test_user1")
+        chan1.add_user(user1)
+        chan1.add_user(serv1.get_user_by_name(serv1.get_nick()))
         try:
             self.function_dispatcher.dispatch("op", user1, chan1)
             data = serv1.get_send_data(1, chan1, Server.MSG_MSG)
-            print(data)
             assert "error" in data[0][0].lower()
             assert "only available for irc" in data[0][0].lower()
         finally:
@@ -30,17 +31,33 @@ class ChannelCapsTest(TestBase, unittest.TestCase):
         self.hallo.add_server(serv1)
         chan1 = serv1.get_channel_by_name("test_chan1")
         user1 = serv1.get_user_by_name("test_user1")
+        chan1.add_user(user1)
+        chan1.add_user(serv1.get_user_by_name(serv1.get_nick()))
         try:
             self.function_dispatcher.dispatch("op", user1, user1)
             data = serv1.get_send_data(1, user1, Server.MSG_MSG)
-            print(data)
             assert "error" in data[0][0].lower()
             assert "in a privmsg" in data[0][0].lower()
         finally:
             self.hallo.remove_server(serv1)
 
     def test_op_0_no_power(self):
-        pass
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        serv1.type = Server.TYPE_IRC
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_name("test_chan1")
+        user1 = serv1.get_user_by_name("test_user1")
+        chan1.add_user(user1)
+        chan1.add_user(serv1.get_user_by_name(serv1.get_nick()))
+        try:
+            self.function_dispatcher.dispatch("op", user1, chan1)
+            data = serv1.get_send_data(1, chan1, Server.MSG_MSG)
+            print(data)
+            assert "error" in data[0][0].lower()
+            assert "don't have power" in data[0][0].lower()
+        finally:
+            self.hallo.remove_server(serv1)
 
     def test_op_0(self):
         pass
