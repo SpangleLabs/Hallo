89c14c568f6d17c42a46725b0d561ed1163baf25
joshcoales <joshua@coales.co.uk>
2016-05-12 10:24:03 +0100

    Empty test class for Operator function

---
+++ test/modules/ChannelControl/testOperator.py
@@ -0,0 +1,64 @@
+import unittest
+
+from Server import Server
+from test.TestBase import TestBase
+
+
+class ChannelCapsTest(TestBase, unittest.TestCase):
+
+    def test_op_not_irc(self):
+        pass
+
+    def test_op_0_privmsg(self):
+        pass
+
+    def test_op_0_no_power(self):
+        pass
+
+    def test_op_0(self):
+        pass
+
+    def test_op_1priv_not_in_channel(self):
+        pass
+
+    def test_op_1priv_no_power(self):
+        pass
+
+    def test_op_1priv(self):
+        pass
+
+    def test_op_1_chan_no_power(self):
+        pass
+
+    def test_op_1_chan(self):
+        pass
+
+    def test_op_1_user_not_here(self):
+        pass
+
+    def test_op_1_user_no_power(self):
+        pass
+
+    def test_op_1_user(self):
+        pass
+
+    def test_op_2_chan_user_not_there(self):
+        pass
+
+    def test_op_2_chan_no_power(self):
+        pass
+
+    def test_op_2_chan(self):
+        pass
+
+    def test_op_2_user_not_in_channel(self):
+        pass
+
+    def test_op_2_user_user_not_there(self):
+        pass
+
+    def test_op_2_user_no_power(self):
+        pass
+
+    def test_op_2_user(self):
+        pass
