b3d55d80765a09a0485f53963e0c2dcfd1e62647
joshcoales <joshua@coales.co.uk>
2016-05-12 14:27:05 +0100

    Implementing OperatorTest.test_op_1priv() test

--- test/modules/ChannelControl/testOperator.py
+++ test/modules/ChannelControl/testOperator.py
@@ -134,7 +134,33 @@ class OperatorTest(TestBase, unittest.TestCase):
             self.hallo.remove_server(serv1)
 
     def test_op_1priv(self):
-        pass
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        serv1.type = Server.TYPE_IRC
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_name("test_chan1")
+        chan1.in_channel = True
+        user1 = serv1.get_user_by_name("test_user1")
+        user_hallo = serv1.get_user_by_name(serv1.get_nick())
+        chan1.add_user(user1)
+        chan1.add_user(user_hallo)
+        chan1_user1 = chan1.get_membership_by_user(user1)
+        chan1_user1.is_op = False
+        chan1_hallo = chan1.get_membership_by_user(user_hallo)
+        chan1_hallo.is_op = True
+        try:
+            self.function_dispatcher.dispatch("op test_chan1", user1, user1)
+            data = serv1.get_send_data(2)
+            assert "error" not in data[0][0].lower()
+            assert data[0][1] is None
+            assert data[1][1] == user1
+            assert data[0][2] == Server.MSG_RAW
+            assert data[1][2] == Server.MSG_MSG
+            assert data[0][0][:4] == "MODE"
+            assert chan1.name+" +o "+user1.name in data[0][0]
+            assert "status given" in data[1][0].lower()
+        finally:
+            self.hallo.remove_server(serv1)
 
     def test_op_1_chan_no_power(self):
         pass
