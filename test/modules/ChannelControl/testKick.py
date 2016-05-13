58dc4c907c9fbb4ed49055b825377524d780b588
joshcoales <joshua@coales.co.uk>
2016-05-13 11:10:32 +0100

    Implementing KickTest.test_kick_3chanusermsg() test

--- test/modules/ChannelControl/testKick.py
+++ test/modules/ChannelControl/testKick.py
@@ -611,7 +611,38 @@ class KickTest(TestBase, unittest.TestCase):
         pass
 
     def test_kick_3chanusermsg(self):
-        pass
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        serv1.type = Server.TYPE_IRC
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_name("test_chan1")
+        chan1.in_channel = True
+        chan2 = serv1.get_channel_by_name("test_chan2")
+        chan2.in_channel = True
+        user1 = serv1.get_user_by_name("test_user1")
+        user2 = serv1.get_user_by_name("test_user2")
+        user_hallo = serv1.get_user_by_name(serv1.get_nick())
+        chan1.add_user(user1)
+        chan1.add_user(user_hallo)
+        chan1_hallo = chan1.get_membership_by_user(user_hallo)
+        chan1_hallo.is_op = True
+        chan2.add_user(user2)
+        chan2.add_user(user_hallo)
+        chan2_hallo = chan2.get_membership_by_user(user_hallo)
+        chan2_hallo.is_op = True
+        try:
+            self.function_dispatcher.dispatch("kick test_chan2 test_user2 goodbye now", user1, chan1)
+            data = serv1.get_send_data(2)
+            assert "error" not in data[0][0].lower()
+            assert data[0][1] is None
+            assert data[1][1] == chan1
+            assert data[0][2] == Server.MSG_RAW
+            assert data[1][2] == Server.MSG_MSG
+            assert data[0][0][:4] == "KICK"
+            assert chan2.name+" "+user2.name+" goodbye now" in data[0][0]
+            assert "kicked "+user2.name+" from "+chan2.name in data[1][0].lower()
+        finally:
+            self.hallo.remove_server(serv1)
 
     def test_kick_3chanmsg_not_in_channel(self):
         pass
