6ab7d5a41861db46825fb2045b18530bc22ae86e
joshcoales <joshua@coales.co.uk>
2016-05-17 21:54:57 +0100

    Adding FindPermissionMaskTest.test_1_channel_privmsg() test

--- test/modules/PermissionControl/testPermissionControl.py
+++ test/modules/PermissionControl/testPermissionControl.py
@@ -182,6 +182,27 @@ class FindPermissionMaskTest(TestBase, unittest.TestCase):
         user1 = serv1.get_user_by_name("test_user1")
         perm2 = PermissionMask()
         user1.permission_mask = perm2
-        # Get permissions of current server
+        # Get permissions of current channel
         data = self.perm_cont.find_permission_mask(["channel"], user1, chan1)
         assert data == perm1, "Did not find the correct permission mask."
+
+    def test_1_channel_privmsg(self):
+        # Set up a test server and channel and user
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        perm0 = PermissionMask()
+        serv1.permission_mask = perm0
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_name("test_chan1")
+        perm1 = PermissionMask()
+        chan1.permission_mask = perm1
+        user1 = serv1.get_user_by_name("test_user1")
+        perm2 = PermissionMask()
+        user1.permission_mask = perm2
+        # Try to get permissions of current channel from a privmsg
+        try:
+            data = self.perm_cont.find_permission_mask(["channel"], user1, None)
+            assert False, "Should not have managed to get permission mask."
+        except modules.PermissionControl.PermissionControlException as e:
+            assert "error" in str(e).lower()
+            assert "can't set generic channel permissions in a privmsg" in str(e).lower()
