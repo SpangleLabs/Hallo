f28490aec1a89f1a16901cd9c41ee67da360795d
joshcoales <joshua@coales.co.uk>
2016-05-17 21:38:16 +0100

    Reorganising FindPermissionMaskTest.test_1_hallo() test

--- test/modules/PermissionControl/testPermissionControl.py
+++ test/modules/PermissionControl/testPermissionControl.py
@@ -1,5 +1,6 @@
 import unittest
 
+from Hallo import Hallo
 from PermissionMask import PermissionMask
 from modules.PermissionControl import Permissions
 from test.ServerMock import ServerMock
@@ -94,12 +95,24 @@ class FindPermissionMaskTest(TestBase, unittest.TestCase):
             assert "server but not channel or user" in str(e).lower()
 
     def test_1_hallo(self):
-        # Set up test info
+        # Set up a test hallo and server and channel and user
+        hallo1 = Hallo()
+        perm3 = PermissionMask()
+        hallo1.permission_mask = perm3
+        serv1 = ServerMock(hallo1)
+        serv1.name = "test_serv1"
+        perm0 = PermissionMask()
+        serv1.permission_mask = perm0
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_name("test_chan1")
         perm1 = PermissionMask()
-        self.hallo.permission_mask = perm1
+        chan1.permission_mask = perm1
+        user1 = serv1.get_user_by_name("test_user1")
+        perm2 = PermissionMask()
+        user1.permission_mask = perm2
         # Get permission of hallo
-        data = self.perm_cont.find_permission_mask(["hallo"], self.test_user, self.test_chan)
-        assert data == perm1, "Did not find the correct permission mask."
+        data = self.perm_cont.find_permission_mask(["hallo"], user1, chan1)
+        assert data == perm3, "Did not find the correct permission mask."
 
     def test_1_server(self):
         # Set up a test server and channel and user
