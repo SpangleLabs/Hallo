052b03b5260fa1af1443b1446b4360e2478a6306
joshcoales <joshua@coales.co.uk>
2016-05-17 21:39:59 +0100

    Adding FindPermissionMaskTest.test_1_server_no_name() test

--- test/modules/PermissionControl/testPermissionControl.py
+++ test/modules/PermissionControl/testPermissionControl.py
@@ -130,3 +130,24 @@ class FindPermissionMaskTest(TestBase, unittest.TestCase):
         # Get permissions of current server
         data = self.perm_cont.find_permission_mask(["server"], user1, chan1)
         assert data == perm0, "Did not find the correct permission mask."
+
+    def test_1_server_no_name(self):
+        # Set up a test server and channel and user
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        perm0 = PermissionMask()
+        serv1.permission_mask = perm0
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_name("test_chan1")
+        perm1 = PermissionMask()
+        chan1.permission_mask = perm1
+        user1 = serv1.get_user_by_name("test_user1")
+        perm2 = PermissionMask()
+        user1.permission_mask = perm2
+        # Get permissions of current server
+        try:
+            data = self.perm_cont.find_permission_mask(["server=test_serv2"], user1, chan1)
+            assert False, "Find permission mask should have failed."
+        except modules.PermissionControl.PermissionControlException as e:
+            assert "error" in str(e).lower()
+            assert "no server exists by that name" in str(e).lower()
