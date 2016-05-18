ca196816269913839bed271718c0905192bd58b4
joshcoales <joshua@coales.co.uk>
2016-05-18 08:44:45 +0100

    Adding FindPermissionMaskTest.test_1_user_group_no_name() test

--- test/modules/PermissionControl/testPermissionControl.py
+++ test/modules/PermissionControl/testPermissionControl.py
@@ -2,6 +2,7 @@ import unittest
 
 from Hallo import Hallo
 from PermissionMask import PermissionMask
+from UserGroup import UserGroup
 from modules.PermissionControl import Permissions
 from test.ServerMock import ServerMock
 from test.TestBase import TestBase
@@ -244,3 +245,31 @@ class FindPermissionMaskTest(TestBase, unittest.TestCase):
         # Get permissions of current channel
         data = self.perm_cont.find_permission_mask(["channel=test_chan2"], user1, chan1)
         assert data == perm4, "Did not find the correct permission mask."
+
+    def test_1_user_group_no_name(self):
+        # Set up a test server and channel and user
+        hallo1 = Hallo()
+        perm3 = PermissionMask()
+        hallo1.permission_mask = perm3
+        serv1 = ServerMock(hallo1)
+        serv1.name = "test_serv1"
+        perm0 = PermissionMask()
+        serv1.permission_mask = perm0
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_name("test_chan1")
+        perm1 = PermissionMask()
+        chan1.permission_mask = perm1
+        user1 = serv1.get_user_by_name("test_user1")
+        perm2 = PermissionMask()
+        user1.permission_mask = perm2
+        group1 = UserGroup("test_group1", hallo1)
+        perm4 = PermissionMask()
+        group1.permission_mask = perm4
+        hallo1.add_user_group(group1)
+        # Get permissions of current server
+        try:
+            data = self.perm_cont.find_permission_mask(["user_group=test_group2"], user1, chan1)
+            assert False, "Find permission mask should have failed."
+        except modules.PermissionControl.PermissionControlException as e:
+            assert "error" in str(e).lower()
+            assert "no user group exists by that name" in str(e).lower()
