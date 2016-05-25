09ea6b9ecb20a248ed08983871e6968c4b616471
joshcoales <joshua@coales.co.uk>
2016-05-25 09:28:18 +0100

    Implementing ActiveThreadsTest.test_threads_increase() test

--- test/modules/HalloControl/testActiveThreads.py
+++ test/modules/HalloControl/testActiveThreads.py
@@ -1,4 +1,7 @@
 import unittest
+from threading import Thread
+
+import time
 
 from Server import Server
 from test.TestBase import TestBase
@@ -13,4 +16,18 @@ class ActiveThreadsTest(TestBase, unittest.TestCase):
         assert "active threads" in data[0][0].lower()
 
     def test_threads_increase(self):
-        pass
+        self.function_dispatcher.dispatch("active threads", self.test_user, self.test_user)
+        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        assert "i have" in data[0][0].lower()
+        assert "active threads" in data[0][0].lower()
+        first_threads = int(data[0][0].lower().split("active")[0].split("have")[1].strip())
+        # Launch 10 threads
+        for _ in range(10):
+            Thread(target=time.sleep, args=(10,)).start()
+        # Run function again
+        self.function_dispatcher.dispatch("active threads", self.test_user, self.test_user)
+        data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+        assert "i have" in data[0][0].lower()
+        assert "active threads" in data[0][0].lower()
+        second_threads = int(data[0][0].lower().split("active")[0].split("have")[1].strip())
+        assert second_threads > first_threads, "Thread count should have increased"
