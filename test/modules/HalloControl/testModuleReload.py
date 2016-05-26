ade670878eab5f1eada97e4362b555bfd8f9a345
joshcoales <joshua@coales.co.uk>
2016-05-26 15:05:35 +0100

    Placeholder test class for ModuleReload module

---
+++ test/modules/HalloControl/testModuleReload.py
@@ -0,0 +1,19 @@
+import unittest
+from threading import Thread
+
+import time
+
+from Server import Server
+from test.TestBase import TestBase
+
+
+class ModuleReloadTest(TestBase, unittest.TestCase):
+
+    def test_module_reload(self):
+        pass
+
+    def test_module_no_name(self):
+        pass
+
+    def test_module_case_insensitive(self):
+        pass
