322eb64070ae19d389f42ac49113f59cb5942c80
joshcoales <joshua@coales.co.uk>
2016-05-27 12:06:36 +0100

    Implementing ModuleReloadTest.test_module_fail() test

--- test/modules/HalloControl/testModuleReload.py
+++ test/modules/HalloControl/testModuleReload.py
@@ -10,6 +10,7 @@ class ModuleReloadTest(TestBase, unittest.TestCase):
         old_func_disp = self.hallo.function_dispatcher
         try:
             mock_func_disp = FunctionDispatcherMock()
+            mock_func_disp.module_reload_resp = True
             self.hallo.function_dispatcher = mock_func_disp
             self.function_dispatcher.dispatch("module reload HalloControl", self.test_user, self.test_user)
             data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
@@ -19,16 +20,25 @@ class ModuleReloadTest(TestBase, unittest.TestCase):
         finally:
             self.hallo.function_dispatcher = old_func_disp
 
-    def test_module_no_name(self):
-        pass
-
-    def test_module_case_insensitive(self):
-        pass
+    def test_module_fail(self):
+        old_func_disp = self.hallo.function_dispatcher
+        try:
+            mock_func_disp = FunctionDispatcherMock()
+            mock_func_disp.module_reload_resp = False
+            self.hallo.function_dispatcher = mock_func_disp
+            self.function_dispatcher.dispatch("module reload HalloControl", self.test_user, self.test_user)
+            data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
+            assert "error" in data[0][0].lower()
+            assert "module reloaded" not in data[0][0].lower()
+            assert not mock_func_disp.module_reloaded
+        finally:
+            self.hallo.function_dispatcher = old_func_disp
 
 
 class FunctionDispatcherMock:
     module_reloaded = False
+    module_reload_resp = True
 
     def reload_module(self, name):
-        self.module_reloaded = True
-        return True
+        self.module_reloaded = self.module_reload_resp
+        return self.module_reload_resp
