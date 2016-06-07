524c87580f4e323222c20ad76d4462360d788c56
joshcoales <joshua@coales.co.uk>
2016-06-07 13:48:52 +0100

    Implementing ConvertPrefixGroupTest.test_xml() test

--- test/modules/Convert/testConvertPrefixGroup.py
+++ test/modules/Convert/testConvertPrefixGroup.py
@@ -13,4 +13,13 @@ class ConvertPrefixGroupTest(unittest.TestCase):
         assert prefix_group.name == "test_group"
 
     def test_xml(self):
+        test_repo = ConvertRepo()
+        prefix_group = ConvertPrefixGroup(test_repo, "test_group")
+        text_xml = prefix_group.to_xml()
+        rebuild_group = ConvertPrefixGroup.from_xml(test_repo, text_xml)
+        assert rebuild_group.repo == test_repo
+        assert len(rebuild_group.prefix_list) == 0
+        assert rebuild_group.name == "test_group"
+
+    def test_xml_contents(self):
         pass
