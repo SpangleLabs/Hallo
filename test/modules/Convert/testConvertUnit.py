f2918e569ce27c12ff0564ebab7625cc311a6c2a
joshcoales <joshua@coales.co.uk>
2016-06-12 17:42:07 +0100

    Placeholder test class for ConvertUnit test class

---
+++ test/modules/Convert/testConvertUnit.py
@@ -0,0 +1,36 @@
+import unittest
+
+from modules.Convert import ConvertPrefix, ConvertPrefixGroup, ConvertRepo
+
+
+class ConvertUnitTest(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_xml(self):
+        pass
+
+    def test_add_name(self):
+        pass
+
+    def test_remove_name(self):
+        pass
+
+    def test_add_abbr(self):
+        pass
+
+    def test_remove_abbr(self):
+        pass
+
+    def test_set_value(self):
+        pass
+
+    def test_set_offset(self):
+        pass
+
+    def test_has_name(self):
+        pass
+
+    def test_get_prefix_from_user_input(self):
+        pass
