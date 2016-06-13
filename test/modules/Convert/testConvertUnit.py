0ea44e75255e077740c3d9fca952533421c56fa1
joshcoales <joshua@coales.co.uk>
2016-06-13 18:55:38 +0100

    Implementing ConvertUnitTest.test_init() test

--- test/modules/Convert/testConvertUnit.py
+++ test/modules/Convert/testConvertUnit.py
@@ -1,12 +1,27 @@
 import unittest
 
-from modules.Convert import ConvertPrefix, ConvertPrefixGroup, ConvertRepo
+from modules.Convert import ConvertRepo, ConvertType, ConvertUnit
 
 
 class ConvertUnitTest(unittest.TestCase):
 
     def test_init(self):
-        pass
+        # Set up test objects
+        test_repo = ConvertRepo()
+        test_type = ConvertType(test_repo, "test_type")
+        test_type.base_unit = ConvertUnit(test_type, ["base_unit"], 1)
+        test_unit_names = ["name1", "name2"]
+        test_value = 1337
+        # Test init
+        test_unit = ConvertUnit(test_type, test_unit_names, test_value)
+        assert len(test_unit.abbr_list) == 0
+        assert test_unit.abbr_list == []
+        assert test_unit.type == test_type
+        assert test_unit.name_list == test_unit_names
+        assert test_unit.value == test_value
+        assert test_unit.offset == 0
+        assert test_unit.last_updated is None
+        assert test_unit.valid_prefix_group is None
 
     def test_xml(self):
         pass
