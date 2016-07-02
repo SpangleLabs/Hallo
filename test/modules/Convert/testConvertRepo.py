45f1701b8afe12c3fa38851491d52e954a7805d5
joshcoales <joshua@coales.co.uk>
2016-07-02 16:58:42 +0100

    Starting on class to test ConvertRepo class

---
+++ test/modules/Convert/testConvertRepo.py
@@ -0,0 +1,33 @@
+import unittest
+
+from modules.Convert import ConvertRepo, ConvertType, ConvertUnit
+
+
+class ConvertRepoTest(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_add_type(self):
+        pass
+
+    def test_remove_type(self):
+        pass
+
+    def test_get_type_by_name(self):
+        pass
+
+    def test_get_full_unit_list(self):
+        pass
+
+    def test_add_prefix_group(self):
+        pass
+
+    def test_remove_prefix_group(self):
+        pass
+
+    def test_get_prefix_group_by_name(self):
+        pass
+
+    def test_xml(self):
+        pass
