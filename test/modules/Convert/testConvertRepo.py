35ce0800267bd85655dd04ec7809eec29ff0fd6c
joshcoales <joshua@coales.co.uk>
2016-07-03 17:24:39 +0100

    Implementing ConvertRepoTest.test_remove_unit() test and extending ConvertRepo.test_add_unit() test

--- test/modules/Convert/testConvertRepo.py
+++ test/modules/Convert/testConvertRepo.py
@@ -1,6 +1,6 @@
 import unittest
 
-from modules.Convert import ConvertRepo, ConvertType, ConvertUnit
+from modules.Convert import ConvertRepo, ConvertType
 
 
 class ConvertRepoTest(unittest.TestCase):
@@ -15,15 +15,37 @@ class ConvertRepoTest(unittest.TestCase):
     def test_add_type(self):
         # Set up test object
         test_repo = ConvertRepo()
-        test_type = ConvertType(test_repo, "test_type")
+        test_type1 = ConvertType(test_repo, "test_type1")
+        test_type2 = ConvertType(test_repo, "test_type2")
         # Add type
-        test_repo.add_type(test_type)
+        test_repo.add_type(test_type1)
         # Check
         assert len(test_repo.type_list) == 1
-        assert test_repo.type_list[0] == test_type
+        assert test_repo.type_list[0] == test_type1
+        # Add another type
+        test_repo.add_type(test_type2)
+        # Check again
+        assert len(test_repo.type_list) == 2
+        assert test_type2 in test_repo.type_list
 
     def test_remove_type(self):
-        pass
+        # Set up test object
+        test_repo = ConvertRepo()
+        test_type1 = ConvertType(test_repo, "test_type1")
+        test_type2 = ConvertType(test_repo, "test_type2")
+        test_repo.add_type(test_type1)
+        test_repo.add_type(test_type2)
+        assert len(test_repo.type_list) == 2
+        # Remove type
+        test_repo.remove_type(test_type1)
+        # Check
+        assert len(test_repo.type_list) == 1
+        assert test_repo.type_list[0] == test_type2
+        # Remove other type
+        test_repo.remove_type(test_type2)
+        # Check again
+        assert len(test_repo.type_list) == 0
+        assert test_repo.type_list == []
 
     def test_get_type_by_name(self):
         pass
