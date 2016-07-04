a9a2f4d5c2445b06d80925185da4aa368e6d7989
joshcoales <joshua@coales.co.uk>
2016-07-04 13:12:35 +0100

    Starting on ConvertMeasureTest test class

---
+++ test/modules/Convert/testConvertMeasure.py
@@ -0,0 +1,27 @@
+import unittest
+
+from modules.Convert import ConvertRepo, ConvertType, ConvertUnit, ConvertPrefixGroup, ConvertMeasure
+
+
+class ConvertMeasureTest(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_is_equal(self):
+        pass
+
+    def test_convert_to(self):
+        pass
+
+    def test_convert_to_base(self):
+        pass
+
+    def test_to_string(self):
+        pass
+
+    def test_to_string_with_prefix(self):
+        pass
+
+    def test_build_list_from_user_input(self):
+        pass
