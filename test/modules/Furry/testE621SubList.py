575d68558c67a13f6d26d1b1dc0d0d0c19dbd85a
joshcoales <joshua@coales.co.uk>
2016-11-04 18:40:25 +0000

    Adding and adjusting all tests for E621SubList, adapted from RssFeedList tests

--- test/modules/Furry/testE621SubList.py
+++ test/modules/Furry/testE621SubList.py
@@ -66,7 +66,7 @@ class TestE621SubList(unittest.TestCase):
         assert rf3 in feed_list
         assert rf5 in feed_list
 
-    def test_get_subs_by_title(self):
+    def test_get_subs_by_search(self):
         serv1 = ServerMock(None)
         serv1.name = "test_serv1"
         serv2 = ServerMock(None)
@@ -80,137 +80,84 @@ class TestE621SubList(unittest.TestCase):
         rf1.search = "butt"
         rf1.server_name = chan1.server.name
         rf1.channel_name = chan1.name
-        rfl.add_feed(rf1)
-        rf2 = RssFeed()
-        rf2.url = "http://spangle.org.uk/hallo/test_feed.xml?2"
-        rf2.title = "test_feed2"
+        rfl.add_sub(rf1)
+        rf2 = E621Sub()
+        rf2.search = "deer"
         rf2.server_name = user2.server.name
         rf2.user_name = user2.name
-        rfl.add_feed(rf2)
-        rf3 = RssFeed()
-        rf3.url = "http://spangle.org.uk/hallo/test_feed.xml?3"
-        rf3.title = "test_feed3"
+        rfl.add_sub(rf2)
+        rf3 = E621Sub()
+        rf3.search = "dragon"
         rf3.server_name = chan3.server.name
         rf3.channel_name = chan3.name
-        rfl.add_feed(rf3)
-        rf4 = RssFeed()
-        rf4.url = "http://spangle.org.uk/hallo/test_feed.xml?4"
-        rf4.title = "test_feed4"
+        rfl.add_sub(rf3)
+        rf4 = E621Sub()
+        rf4.search = "fox"
         rf4.server_name = chan3.server.name
         rf4.channel_name = chan3.name
-        rfl.add_feed(rf4)
-        rf5 = RssFeed()
-        rf5.url = "http://spangle.org.uk/hallo/test_feed.xml?5"
-        rf5.title = "test_feed3"
+        rfl.add_sub(rf4)
+        rf5 = E621Sub()
+        rf5.search = "dragon"
         rf5.server_name = chan3.server.name
         rf5.channel_name = chan3.name
-        rfl.add_feed(rf5)
+        rfl.add_sub(rf5)
         # Check function
-        feed_list = rfl.get_feeds_by_title("test_feed3", chan3)
+        feed_list = rfl.get_subs_by_search("test_feed3", chan3)
         assert len(feed_list) == 2
         assert rf3 in feed_list
         assert rf5 in feed_list
 
-    def test_get_feeds_by_url(self):
-        serv1 = ServerMock(None)
-        serv1.name = "test_serv1"
-        serv2 = ServerMock(None)
-        serv2.name = "test_serv2"
-        chan1 = serv1.get_channel_by_name("test_chan1")
-        user2 = serv1.get_user_by_name("test_user2")
-        chan3 = serv2.get_channel_by_name("test_chan3")
+    def test_remove_sub(self):
         # Setup a feed list
-        rfl = RssFeedList()
-        rf1 = RssFeed()
-        rf1.url = "http://spangle.org.uk/hallo/test_feed.xml?1"
-        rf1.title = "test_feed1"
-        rf1.server_name = chan1.server.name
-        rf1.channel_name = chan1.name
-        rfl.add_feed(rf1)
-        rf2 = RssFeed()
-        rf2.url = "http://spangle.org.uk/hallo/test_feed.xml?2"
-        rf2.title = "test_feed2"
-        rf2.server_name = user2.server.name
-        rf2.user_name = user2.name
-        rfl.add_feed(rf2)
-        rf3 = RssFeed()
-        rf3.url = "http://spangle.org.uk/hallo/test_feed.xml?3"
-        rf3.title = "test_feed3"
-        rf3.server_name = chan3.server.name
-        rf3.channel_name = chan3.name
-        rfl.add_feed(rf3)
-        rf4 = RssFeed()
-        rf4.url = "http://spangle.org.uk/hallo/test_feed.xml?4"
-        rf4.title = "test_feed4"
-        rf4.server_name = chan3.server.name
-        rf4.channel_name = chan3.name
-        rfl.add_feed(rf4)
-        rf5 = RssFeed()
-        rf5.url = "http://spangle.org.uk/hallo/test_feed.xml?4"
-        rf5.title = "test_feed3"
-        rf5.server_name = chan3.server.name
-        rf5.channel_name = chan3.name
-        rfl.add_feed(rf5)
-        # Check function
-        feed_list = rfl.get_feeds_by_url("http://spangle.org.uk/hallo/test_feed.xml?4", chan3)
-        assert len(feed_list) == 2
-        assert rf4 in feed_list
-        assert rf5 in feed_list
-
-    def test_remove_feed(self):
-        # Setup a feed list
-        rfl = RssFeedList()
-        rf1 = RssFeed()
-        rf1.url = "http://spangle.org.uk/hallo/test_feed.xml?1"
-        rfl.add_feed(rf1)
-        rf2 = RssFeed()
-        rf2.url = "http://spangle.org.uk/hallo/test_feed.xml?2"
-        rfl.add_feed(rf2)
-        assert len(rfl.feed_list) == 2
+        rfl = E621SubList()
+        rf1 = E621Sub()
+        rf1.search = "butt"
+        rfl.add_sub(rf1)
+        rf2 = E621Sub()
+        rf2.search = "deer"
+        rfl.add_sub(rf2)
+        assert len(rfl.sub_list) == 2
         # Remove an item from the feed list
-        rfl.remove_feed(rf1)
-        assert len(rfl.feed_list) == 1
-        assert rfl.feed_list[0] == rf2
+        rfl.remove_sub(rf1)
+        assert len(rfl.sub_list) == 1
+        assert rfl.sub_list[0] == rf2
 
     def test_xml(self):
         # Setup a feed list
-        rfl = RssFeedList()
-        rf1 = RssFeed()
-        rf1.url = "http://spangle.org.uk/hallo/test_feed.xml?1"
-        rf1.title = "test_feed1"
+        rfl = E621SubList()
+        rf1 = E621Sub()
+        rf1.search = "butt"
         rf1.update_frequency = Commons.load_time_delta("P0T3600S")
         rf1.server_name = "test_serv1"
         rf1.channel_name = "test_chan1"
-        rfl.add_feed(rf1)
-        rf2 = RssFeed()
-        rf2.url = "http://spangle.org.uk/hallo/test_feed.xml?2"
-        rf2.title = "test_feed2"
+        rfl.add_sub(rf1)
+        rf2 = E621Sub()
+        rf2.search = "deer"
         rf2.update_frequency = Commons.load_time_delta("P1TS")
         rf2.server_name = "test_serv2"
         rf2.channel_name = "test_chan2"
-        rfl.add_feed(rf2)
-        rf3 = RssFeed()
-        rf3.url = "http://spangle.org.uk/hallo/test_feed.xml?3"
-        rf3.title = "test_feed3"
+        rfl.add_sub(rf2)
+        rf3 = E621Sub()
+        rf3.search = "dragon"
         rf3.update_frequency = Commons.load_time_delta("PT60S")
         rf3.server_name = "test_serv3"
         rf3.user_name = "test_user3"
-        rfl.add_feed(rf3)
+        rfl.add_sub(rf3)
         # Save to XML and load
         try:
             try:
-                os.rename("store/rss_feeds.xml", "store/rss_feeds.xml.tmp")
+                os.rename("store/e621_subscriptions.xml", "store/e621_subscriptions.xml.tmp")
             except OSError:
                 pass
             rfl.to_xml()
-            new_rfl = RssFeedList.from_xml()
-            assert len(new_rfl.feed_list) == 3
+            new_rfl = E621SubList.from_xml()
+            assert len(new_rfl.sub_list) == 3
         finally:
             try:
-                os.remove("store/rss_feeds.xml")
+                os.remove("store/e621_subscriptions.xml")
             except OSError:
                 pass
             try:
-                os.rename("store/rss_feeds.xml.tmp", "store/rss_feeds.xml")
+                os.rename("store/e621_subscriptions.xml.tmp", "store/e621_subscriptions.xml")
             except OSError:
                 pass
