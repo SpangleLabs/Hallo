b4140094baf99b5be07ae5c46eefebb48de0f240
joshcoales <joshua@coales.co.uk>
2016-11-11 10:56:57 +0000

    Tweaking some stuff in test_fd_load_order() and test_init()

--- test/testFunctionDispatcher.py
+++ test/testFunctionDispatcher.py
@@ -13,10 +13,10 @@ class FunctionDispatcherTest(TestBase, unittest.TestCase):
         # Add modules to allowed list
         fd.module_list = {"Euler", "Math"}
         # Load up Euler module, ensure no other modules load.
-        fd.reload_module("Euler")
+        assert fd.reload_module("Euler")
         assert len(fd.function_dict) == 1
         # Load second module, ensure all methods are there.
-        fd.reload_module("Math")
+        assert fd.reload_module("Math")
         assert len(fd.function_dict) == 2
 
     def test_fd_disallowed_module(self):
@@ -27,10 +27,13 @@ class FunctionDispatcherTest(TestBase, unittest.TestCase):
 
     def test_init(self):
         # Create some basic stuff
-        test_modules = {"Euler", "Math", "Convert"}
+        test_modules = {"Euler"}
         test_hallo = Hallo()
         # Create function dispatcher
         fd = FunctionDispatcher(test_modules, test_hallo)
         # Check basic class variable setting
-        assert fd.hallo == test_hallo, "Hallo object was not set correctly in FunctionDispatcher"
-        assert fd.module_list == test_modules
+        assert fd.hallo == test_hallo, "Hallo object was not set correctly in FunctionDispatcher."
+        assert fd.module_list == test_modules, "Module list was not imported correctly by FunctionDispatcher."
+        # Check that module reloading has done things
+        assert len(fd.function_dict) == len(test_modules), "Modules were not loaded to function dictionary."
+        assert len(fd.function_names) != 0, "Functions were not added to function_names"
