1acb2611ce83de08e191284de0bfa9d85eac4e7e
joshcoales <joshua@coales.co.uk>
2016-11-11 10:33:26 +0000

    Adding FunctionDispatcherTest.test_init() method, also cleaning up other tests in class

--- test/testFunctionDispatcher.py
+++ test/testFunctionDispatcher.py
@@ -1,6 +1,7 @@
 import unittest
 
 from FunctionDispatcher import FunctionDispatcher
+from Hallo import Hallo
 from test.TestBase import TestBase
 
 
@@ -8,9 +9,9 @@ class FunctionDispatcherTest(TestBase, unittest.TestCase):
 
     def test_fd_load_order(self):
         # Create a blank function dispatcher
-        fd = FunctionDispatcher([], None)
+        fd = FunctionDispatcher(set(), self.hallo)
         # Add modules to allowed list
-        fd.module_list = ["Euler", "Math"]
+        fd.module_list = {"Euler", "Math"}
         # Load up Euler module, ensure no other modules load.
         fd.reload_module("Euler")
         assert len(fd.function_dict) == 1
@@ -20,6 +21,16 @@ class FunctionDispatcherTest(TestBase, unittest.TestCase):
 
     def test_fd_disallowed_module(self):
         # Create a blank function dispatcher
-        fd = FunctionDispatcher([], None)
+        fd = FunctionDispatcher(set(), self.hallo)
         # Try and load a module
         assert not fd.reload_module("Euler")
+
+    def test_init(self):
+        # Create some basic stuff
+        test_modules = {"Euler", "Math", "Convert"}
+        test_hallo = Hallo()
+        # Create function dispatcher
+        fd = FunctionDispatcher(test_modules, test_hallo)
+        # Check basic class variable setting
+        assert fd.hallo == test_hallo, "Hallo object was not set correctly in FunctionDispatcher"
+        assert fd.module_list == test_modules
