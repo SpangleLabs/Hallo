abe0970f025eddc7bb09b316031d80e51ebc25c1
joshcoales <joshua@coales.co.uk>
2016-11-11 00:09:28 +0000

    Some quick tests for the FunctionDispatcher

---
+++ test/testFunctionDispatcher.py
@@ -0,0 +1,30 @@
+import unittest
+
+from FunctionDispatcher import FunctionDispatcher
+from Hallo import Hallo
+from test.TestBase import TestBase
+
+
+class FunctionDispatcherTest(TestBase, unittest.TestCase):
+
+    def test_fd_load_order(self):
+        # Create a Hallo
+        self.hallo = Hallo()
+        # Create a blank function dispatcher
+        fd = FunctionDispatcher([], None)
+        # Add modules to allowed list
+        fd.module_list = ["Euler", "Math"]
+        # Load up Euler module, ensure no other modules load.
+        fd.reload_module("Euler")
+        assert len(fd.function_dict) == 1
+        # Load second module, ensure all methods are there.
+        fd.reload_module("Math")
+        assert len(fd.function_dict) == 2
+
+    def test_fd_disallowed_module(self):
+        # Create a hallo
+        self.hallo = Hallo()
+        # Create a blank function dispatcher
+        fd = FunctionDispatcher([], None)
+        # Try and load a module
+        assert not fd.reload_module("Euler")
