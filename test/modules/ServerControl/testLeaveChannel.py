854d02779be388ce5168b972a22aaaf150b33426
joshcoales <joshua@coales.co.uk>
2016-12-01 15:01:15 +0000

    Added test_other_channel_name() test

--- test/modules/ServerControl/testLeaveChannel.py
+++ test/modules/ServerControl/testLeaveChannel.py
@@ -27,3 +27,12 @@ class LeaveChannelTest(TestBase, unittest.TestCase):
         data = self.server.get_send_data(1, self.test_user, Server.MSG_MSG)
         self.server.get_left_channels(0)
         assert "error" in data[0][0].lower()
+
+    def test_other_channel_name(self):
+        other = self.server.get_channel_by_name("#other")
+        self.function_dispatcher.dispatch("leave "+other.name, self.test_user, self.test_chan)
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        chans = self.server.get_left_channels(1)
+        assert chans[0] == other
+        assert "left" in data[0][0].lower()
+        assert other.name in data[0][0].lower()
