9cb333e668ad8f9fb4a2baa512fec5e5851ef605
joshcoales <joshua@coales.co.uk>
2016-12-01 14:26:38 +0000

    Fixing up tests in LeaveChannelTest to function and use ServerMock.get_left_channels()

--- test/modules/ServerControl/testLeaveChannel.py
+++ test/modules/ServerControl/testLeaveChannel.py
@@ -1,7 +1,6 @@
 import unittest
 
-import time
-
+from Server import Server
 from test.TestBase import TestBase
 
 
@@ -9,16 +8,13 @@ class LeaveChannelTest(TestBase, unittest.TestCase):
 
     def test_no_args(self):
         self.function_dispatcher.dispatch("leave", self.test_user, self.test_chan)
-        time.sleep(5)
-        data = self.server.get_send_data(2)
-        time.sleep(5)
-        assert data[0][1] is None
-        assert data[1][1] == self.test_chan
+        data = self.server.get_send_data(1, self.test_chan, Server.MSG_MSG)
+        chans = self.server.get_left_channels(1)
+        assert chans[0] == self.test_chan
+        assert "left" in data[0][0].lower()
+        assert self.test_chan.name in data[0][0].lower()
 
     def test_channel_name(self):
         self.function_dispatcher.dispatch("leave "+self.test_chan.get_name(), self.test_user, self.test_chan)
-        time.sleep(5)
-        data = self.server.get_send_data(2)
-        time.sleep(5)
-        assert data[0][1] is None
-        assert data[1][1] == self.test_chan
+        data = self.server.get_send_data(1)
+        assert data[0][1] == self.test_chan
