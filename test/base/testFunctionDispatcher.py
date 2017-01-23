4a8322c3a91803eac143893648b145bfbbb9d38a
joshcoales <joshua@coales.co.uk>
2017-01-23 15:01:12 +0000

    Ensure function dispatchers are closed properly by tests

--- test/base/testFunctionDispatcher.py
+++ test/base/testFunctionDispatcher.py
@@ -10,20 +10,26 @@ class FunctionDispatcherTest(TestBase, unittest.TestCase):
     def test_fd_load_order(self):
         # Create a blank function dispatcher
         fd = FunctionDispatcher(set(), self.hallo)
-        # Add modules to allowed list
-        fd.module_list = {"Euler", "Math"}
-        # Load up Euler module, ensure no other modules load.
-        assert fd.reload_module("Euler")
-        assert len(fd.function_dict) == 1
-        # Load second module, ensure all methods are there.
-        assert fd.reload_module("Math")
-        assert len(fd.function_dict) == 2
+        try:
+            # Add modules to allowed list
+            fd.module_list = {"Euler", "Math"}
+            # Load up Euler module, ensure no other modules load.
+            assert fd.reload_module("Euler")
+            assert len(fd.function_dict) == 1
+            # Load second module, ensure all methods are there.
+            assert fd.reload_module("Math")
+            assert len(fd.function_dict) == 2
+        finally:
+            fd.close()
 
     def test_fd_disallowed_module(self):
         # Create a blank function dispatcher
         fd = FunctionDispatcher(set(), self.hallo)
-        # Try and load a module
-        assert not fd.reload_module("Euler")
+        try:
+            # Try and load a module
+            assert not fd.reload_module("Euler")
+        finally:
+            fd.close()
 
     def test_init(self):
         # Create some basic stuff
@@ -31,9 +37,14 @@ class FunctionDispatcherTest(TestBase, unittest.TestCase):
         test_hallo = Hallo()
         # Create function dispatcher
         fd = FunctionDispatcher(test_modules, test_hallo)
-        # Check basic class variable setting
-        assert fd.hallo == test_hallo, "Hallo object was not set correctly in FunctionDispatcher."
-        assert fd.module_list == test_modules, "Module list was not imported correctly by FunctionDispatcher."
-        # Check that module reloading has done things
-        assert len(fd.function_dict) == len(test_modules), "Modules were not loaded to function dictionary."
-        assert len(fd.function_names) != 0, "Functions were not added to function_names"
+        test_hallo.function_dispatcher = fd
+        try:
+            # Check basic class variable setting
+            assert fd.hallo == test_hallo, "Hallo object was not set correctly in FunctionDispatcher."
+            assert fd.module_list == test_modules, "Module list was not imported correctly by FunctionDispatcher."
+            # Check that module reloading has done things
+            assert len(fd.function_dict) == len(test_modules), "Modules were not loaded to function dictionary."
+            assert len(fd.function_names) != 0, "Functions were not added to function_names"
+        finally:
+            fd.close()
+            test_hallo.close()
