5735e1ccb85c425a40f07c4deb4057bf45eeda9b
joshcoales <joshua@coales.co.uk>
2017-01-23 16:39:00 +0000

    Adding test for FunctionDispatcher.to_xml() and FunctionDispatcher.from_xml(), in test_xml() test

--- test/base/testFunctionDispatcher.py
+++ test/base/testFunctionDispatcher.py
@@ -49,6 +49,20 @@ class FunctionDispatcherTest(TestBase, unittest.TestCase):
             fd.close()
             test_hallo.close()
 
+    def test_xml(self):
+        # Set up
+        test_modules = {"Euler", "Bio"}
+        fd = FunctionDispatcher(test_modules, self.hallo)
+        # Get xml output
+        xml_fd = fd.to_xml()
+        # Create new function dispatcher from the xml
+        fd2 = FunctionDispatcher.from_xml(xml_fd, self.hallo)
+        # Check it's the same
+        assert len(fd.module_list) == len(fd2.module_list)
+        for module in fd2.module_list:
+            assert module in fd.module_list
+        assert fd2.hallo == self.hallo
+
 # TODO: write tests for each method:
 # dispatch
 # dispatch_passive
@@ -63,5 +77,3 @@ class FunctionDispatcherTest(TestBase, unittest.TestCase):
 # load_function
 # unload_function
 # close
-# to_xml
-# from_xml
