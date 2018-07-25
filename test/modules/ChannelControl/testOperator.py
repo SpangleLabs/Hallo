9b4ade519a44bb48c500d20826db8828d7c75cda
Joshua Coales <joshua.coales@featurespace.co.uk>
2018-07-25 14:14:09 +0100

    Fixing Operator function and tests

--- test/modules/ChannelControl/testOperator.py
+++ test/modules/ChannelControl/testOperator.py
@@ -111,12 +111,35 @@ class OperatorTest(TestBase, unittest.TestCase):
         finally:
             self.hallo.remove_server(serv1)
 
+    def test_op_1priv_not_known(self):
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        serv1.type = Server.TYPE_IRC
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_address("test_chan1".lower(), "test_chan1")
+        user1 = serv1.get_user_by_address("test_user1".lower(), "test_user1")
+        user_hallo = serv1.get_user_by_address(serv1.get_nick().lower(), serv1.get_nick())
+        chan1.add_user(user1)
+        chan1.add_user(user_hallo)
+        chan1_user1 = chan1.get_membership_by_user(user1)
+        chan1_user1.is_op = False
+        chan1_hallo = chan1.get_membership_by_user(user_hallo)
+        chan1_hallo.is_op = True
+        try:
+            self.function_dispatcher.dispatch("op other_channel", user1, user1)
+            data = serv1.get_send_data(1, user1, Server.MSG_MSG)
+            assert "error" in data[0][0].lower()
+            assert "other_channel is not known" in data[0][0].lower()
+        finally:
+            self.hallo.remove_server(serv1)
+
     def test_op_1priv_not_in_channel(self):
         serv1 = ServerMock(self.hallo)
         serv1.name = "test_serv1"
         serv1.type = Server.TYPE_IRC
         self.hallo.add_server(serv1)
         chan1 = serv1.get_channel_by_address("test_chan1".lower(), "test_chan1")
+        serv1.get_channel_by_address("other_channel", "other_channel")
         user1 = serv1.get_user_by_address("test_user1".lower(), "test_user1")
         user_hallo = serv1.get_user_by_address(serv1.get_nick().lower(), serv1.get_nick())
         chan1.add_user(user1)
@@ -473,6 +496,38 @@ class OperatorTest(TestBase, unittest.TestCase):
         finally:
             self.hallo.remove_server(serv1)
 
+    def test_op_2_chan_user_not_known(self):
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        serv1.type = Server.TYPE_IRC
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_address("test_chan1".lower(), "test_chan1")
+        chan1.in_channel = True
+        chan2 = serv1.get_channel_by_address("test_chan2".lower(), "test_chan2")
+        chan2.in_channel = True
+        user1 = serv1.get_user_by_address("test_user1".lower(), "test_user1")
+        user2 = serv1.get_user_by_address("test_user2".lower(), "test_user2")
+        user_hallo = serv1.get_user_by_address(serv1.get_nick().lower(), serv1.get_nick())
+        chan1.add_user(user1)
+        chan1_user1 = chan1.get_membership_by_user(user1)
+        chan1_user1.is_op = False
+        chan1.add_user(user_hallo)
+        chan1_hallo = chan1.get_membership_by_user(user_hallo)
+        chan1_hallo.is_op = True
+        chan2.add_user(user2)
+        chan2_user1 = chan2.get_membership_by_user(user2)
+        chan2_user1.is_op = False
+        chan2.add_user(user_hallo)
+        chan2_hallo = chan2.get_membership_by_user(user_hallo)
+        chan2_hallo.is_op = True
+        try:
+            self.function_dispatcher.dispatch("op test_chan2 test_user3", user1, chan1)
+            data = serv1.get_send_data(1, chan1, Server.MSG_MSG)
+            assert "error" in data[0][0].lower()
+            assert "test_user3 is not known" in data[0][0].lower()
+        finally:
+            self.hallo.remove_server(serv1)
+
     def test_op_2_chan_user_not_there(self):
         serv1 = ServerMock(self.hallo)
         serv1.name = "test_serv1"
@@ -484,6 +539,7 @@ class OperatorTest(TestBase, unittest.TestCase):
         chan2.in_channel = True
         user1 = serv1.get_user_by_address("test_user1".lower(), "test_user1")
         user2 = serv1.get_user_by_address("test_user2".lower(), "test_user2")
+        serv1.get_user_by_address("test_user3", "test_user3")
         user_hallo = serv1.get_user_by_address(serv1.get_nick().lower(), serv1.get_nick())
         chan1.add_user(user1)
         chan1_user1 = chan1.get_membership_by_user(user1)
@@ -636,6 +692,38 @@ class OperatorTest(TestBase, unittest.TestCase):
         finally:
             self.hallo.remove_server(serv1)
 
+    def test_op_2_user_user_not_known(self):
+        serv1 = ServerMock(self.hallo)
+        serv1.name = "test_serv1"
+        serv1.type = Server.TYPE_IRC
+        self.hallo.add_server(serv1)
+        chan1 = serv1.get_channel_by_address("test_chan1".lower(), "test_chan1")
+        chan1.in_channel = True
+        chan2 = serv1.get_channel_by_address("test_chan2".lower(), "test_chan2")
+        chan2.in_channel = True
+        user1 = serv1.get_user_by_address("test_user1".lower(), "test_user1")
+        user2 = serv1.get_user_by_address("test_user2".lower(), "test_user2")
+        user_hallo = serv1.get_user_by_address(serv1.get_nick().lower(), serv1.get_nick())
+        chan1.add_user(user1)
+        chan1_user1 = chan1.get_membership_by_user(user1)
+        chan1_user1.is_op = False
+        chan1.add_user(user_hallo)
+        chan1_hallo = chan1.get_membership_by_user(user_hallo)
+        chan1_hallo.is_op = True
+        chan2.add_user(user2)
+        chan2_user1 = chan2.get_membership_by_user(user2)
+        chan2_user1.is_op = False
+        chan2.add_user(user_hallo)
+        chan2_hallo = chan2.get_membership_by_user(user_hallo)
+        chan2_hallo.is_op = True
+        try:
+            self.function_dispatcher.dispatch("op test_user3 test_chan2", user1, chan1)
+            data = serv1.get_send_data(1, chan1, Server.MSG_MSG)
+            assert "error" in data[0][0].lower()
+            assert "test_user3 is not known" in data[0][0].lower()
+        finally:
+            self.hallo.remove_server(serv1)
+
     def test_op_2_user_user_not_there(self):
         serv1 = ServerMock(self.hallo)
         serv1.name = "test_serv1"
@@ -647,6 +735,7 @@ class OperatorTest(TestBase, unittest.TestCase):
         chan2.in_channel = True
         user1 = serv1.get_user_by_address("test_user1".lower(), "test_user1")
         user2 = serv1.get_user_by_address("test_user2".lower(), "test_user2")
+        serv1.get_user_by_address("test_user3", "test_user3")
         user_hallo = serv1.get_user_by_address(serv1.get_nick().lower(), serv1.get_nick())
         chan1.add_user(user1)
         chan1_user1 = chan1.get_membership_by_user(user1)
