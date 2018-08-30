7a95f10943d8099b316bea64d14e3a9d057ea274
joshcoales <joshua@coales.co.uk>
2018-08-30 23:39:34 +0100

    Swapping Pony Functions to return Events

--- modules/Pony.py
+++ modules/Pony.py
@@ -1,8 +1,7 @@
-from Events import EventMessage
+from Events import EventMessage, EventCTCP
 from Function import Function
 from xml.dom import minidom
 
-from Server import Server
 from inc.Commons import Commons
 
 
@@ -44,7 +43,7 @@ class PonyEpisode(Function):
         else:
             episode = Commons.get_random_choice(song_list)[0]
         # Return output
-        return "You should choose: {} - {}.".format(episode['full_code'], episode['name'])
+        return event.create_response("You should choose: {} - {}.".format(episode['full_code'], episode['name']))
 
 
 class BestPony(Function):
@@ -101,7 +100,7 @@ class BestPony(Function):
         output_message = random_half_1.replace("{X}",
                                                chosen_pony['name']) + random_half_2.replace("{Y}",
                                                                                             chosen_pony['pronoun'])
-        return output_message
+        return event.create_response(output_message)
 
     def get_passive_events(self):
         """Returns a list of events which this function may want to respond to in a passive way"""
@@ -139,20 +138,20 @@ class Cupcake(Function):
         Gives out cupcakes (much better than muffins.) Format: cupcake <username> <type>
         """
         if event.command_args.strip() == '':
-            return "You must specify a recipient for the cupcake."
+            return event.create_response("You must specify a recipient for the cupcake.")
         # Get some required objects
         server_obj = event.server
         recipient_user_name = event.command_args.split()[0]
         recipient_user_obj = server_obj.get_user_by_name(recipient_user_name)
         # If user isn't online, I can't send a cupcake
         if not recipient_user_obj.online:
-            return "No one called {} is online.".format(recipient_user_name)
+            return event.create_response("No one called {} is online.".format(recipient_user_name))
         # Generate the output message, adding cupcake type if required
         if recipient_user_name == event.command_args.strip():
-            output_message = "\x01ACTION gives {} a cupcake, from {}.\x01".format(recipient_user_name, event.user.name)
+            output_message = "ACTION gives {} a cupcake, from {}.".format(recipient_user_name, event.user.name)
         else:
             cupcake_type = event.command_args[len(recipient_user_name):].strip()
-            output_message = "\x01ACTION gives {} a {} cupcake, from {}.\x01".format(recipient_user_name, cupcake_type,
+            output_message = "ACTION gives {} a {} cupcake, from {}.".format(recipient_user_name, cupcake_type,
                                                                                      event.user.name)
         # Get both users channel lists, and then the intersection
         user_channel_list = event.user.get_channel_list()
@@ -160,20 +159,20 @@ class Cupcake(Function):
         intersection_list = user_channel_list.intersection(recipient_channel_list)
         # If current channel is in the intersection, send there.
         if event.channel in intersection_list:
-            return output_message
+            return event.create_response(output_message)
         # Get list of channels that hallo is in inside that intersection
         valid_channels = [chan for chan in intersection_list if chan.in_channel]
         # If length of valid channel list is nonzero, pick a channel and send.
         if len(valid_channels) != 0:
             chosen_channel = Commons.get_random_choice(valid_channels)[0]
-            server_obj.send(output_message, chosen_channel, Server.MSG_MSG)
-            return "Cupcake sent."
+            server_obj.send(EventCTCP(server_obj, chosen_channel, None, output_message, inbound=False))
+            return event.create_response("Cupcake sent.")
         # If no valid intersection channels, see if there are any valid recipient channels
         valid_channels = [chan for chan in recipient_channel_list if chan.in_channel]
         if len(valid_channels) != 0:
             chosen_channel = Commons.get_random_choice(valid_channels)[0]
-            server_obj.send(output_message, chosen_channel, Server.MSG_MSG)
-            return "Cupcake sent."
+            server_obj.send(EventCTCP(server_obj, chosen_channel, None, output_message, inbound=False))
+            return event.create_response("Cupcake sent.")
         # Otherwise, use privmsg
-        server_obj.send(output_message, recipient_user_obj, Server.MSG_MSG)
-        return "Cupcake sent."
+        server_obj.send(EventCTCP(server_obj, None, recipient_user_obj, output_message, inbound=False))
+        return event.create_response("Cupcake sent.")
