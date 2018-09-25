af3a1fb750330f680477377ac0d641d9bae6fafe
joshcoales <joshua@coales.co.uk>
2018-09-25 11:13:40 +0100

    Adding FANotificationCommentsSub to test class

--- test/modules/Subscriptions/testAllSubscriptionTypes.py
+++ test/modules/Subscriptions/testAllSubscriptionTypes.py
@@ -6,7 +6,8 @@ import unittest
 import modules
 from Events import EventMessage
 from modules.Subscriptions import SubscriptionFactory, E621Sub, RssSub, FANotificationNotesSub, FASearchSub, FAKey, \
-    SubscriptionRepo, FAKeysCommon, FAUserFavsSub, FAUserWatchersSub, FANotificationWatchSub, FANotificationFavSub
+    SubscriptionRepo, FAKeysCommon, FAUserFavsSub, FAUserWatchersSub, FANotificationWatchSub, FANotificationFavSub, \
+    FANotificationCommentsSub
 
 from test.TestBase import TestBase
 
@@ -26,6 +27,7 @@ class TestAllSubscriptionClasses(TestBase, unittest.TestCase):
         sub_objs.append(FAUserWatchersSub(self.server, self.test_chan, fa_key, "zephyr42"))
         sub_objs.append(FANotificationWatchSub(self.server, self.test_chan, fa_key))
         sub_objs.append(FANotificationFavSub(self.server, self.test_chan, fa_key))
+        sub_objs.append(FANotificationCommentsSub(self.server, self.test_chan, fa_key))
         return sub_objs
 
     def get_sub_create_events(self):
@@ -51,6 +53,8 @@ class TestAllSubscriptionClasses(TestBase, unittest.TestCase):
         sub_evts[FANotificationWatchSub].command_args = ""
         sub_evts[FANotificationFavSub] = EventMessage(self.server, self.test_chan, self.test_user, "")
         sub_evts[FANotificationFavSub].command_args = ""
+        sub_evts[FANotificationCommentsSub] = EventMessage(self.server, self.test_chan, self.test_user, "")
+        sub_evts[FANotificationCommentsSub].command_args = ""
         return sub_evts
 
     def test_all_sub_classes_in_sub_objs(self):
