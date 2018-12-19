b3ba7da2ddd48a6148b8cafcaad6e1cf395868e1
joshcoales <joshua@coales.co.uk>
2018-12-19 22:15:31 +0000

    Adding some DailysFAFieldTest stuff

--- test/modules/Dailys/fields/testDailysFAField.py
+++ test/modules/Dailys/fields/testDailysFAField.py
@@ -1,7 +1,32 @@
 import unittest
 
+from Events import EventMessage
+from modules.Dailys import DailysFAField, DailysException
+from test.TestBase import TestBase
+from test.modules.Dailys.DailysSpreadsheetMock import DailysSpreadsheetMock
 
-class DailysFAFieldTest(unittest.TestCase):
+
+class DailysFAFieldTest(TestBase, unittest.TestCase):
 
     def test_day_rollover(self):
+        pass
+
+    def test_create_from_input_no_fa_data(self):
+        # Setup
+        evt = EventMessage(self.server, self.test_chan, self.test_user, "setup FA dailys field")
+        spreadsheet = DailysSpreadsheetMock(self.test_user, self.test_chan)
+        # Create from input
+        try:
+            DailysFAField.create_from_input(evt, spreadsheet)
+            assert False, "Should have failed to create DailysFAField due to missing FA login info."
+        except DailysException as e:
+            assert "no fa data" in str(e).lower(), "Exception did not mention that there was no FA data set up."
+
+    def test_create_from_input_with_column_specified(self):
+        pass
+
+    def test_create_from_input_with_column_found(self):
+        pass
+
+    def test_create_from_input_with_column_not_found(self):
         pass
