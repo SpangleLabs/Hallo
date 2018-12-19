1b1eb1eb189b2d9409958b7da272e12eb8d234e5
joshcoales <joshua@coales.co.uk>
2018-12-19 22:27:13 +0000

    Implementing DailysSpreadsheetMock.tag_column() and DailysFAFieldTest.test_create_from_input_with_column_specified()

--- test/modules/Dailys/DailysSpreadsheetMock.py
+++ test/modules/Dailys/DailysSpreadsheetMock.py
@@ -3,6 +3,12 @@ from modules.Dailys import DailysSpreadsheet
 
 class DailysSpreadsheetMock(DailysSpreadsheet):
 
+    test_column_key = "abcdefg"
+
     def __init__(self, user, destination):
         super().__init__(user, destination, None)
+        self.tagged_columns = []
 
+    def tag_column(self, col_title):
+        self.tagged_columns.append(col_title)
+        return self.test_column_key
--- test/modules/Dailys/fields/testDailysFAField.py
+++ test/modules/Dailys/fields/testDailysFAField.py
@@ -2,6 +2,7 @@ import unittest
 
 from Events import EventMessage
 from modules.Dailys import DailysFAField, DailysException
+from modules.UserData import UserDataParser, FAKeyData
 from test.TestBase import TestBase
 from test.modules.Dailys.DailysSpreadsheetMock import DailysSpreadsheetMock
 
@@ -13,7 +14,10 @@ class DailysFAFieldTest(TestBase, unittest.TestCase):
 
     def test_create_from_input_no_fa_data(self):
         # Setup
-        evt = EventMessage(self.server, self.test_chan, self.test_user, "setup FA dailys field")
+        cmd_name = "setup dailys field"
+        cmd_args = "furaffinity AF"
+        evt = EventMessage(self.server, self.test_chan, self.test_user, "{} {}".format(cmd_name, cmd_args))
+        evt.split_command_text(cmd_name, cmd_args)
         spreadsheet = DailysSpreadsheetMock(self.test_user, self.test_chan)
         # Create from input
         try:
@@ -23,7 +27,20 @@ class DailysFAFieldTest(TestBase, unittest.TestCase):
             assert "no fa data" in str(e).lower(), "Exception did not mention that there was no FA data set up."
 
     def test_create_from_input_with_column_specified(self):
-        pass
+        # Setup
+        cmd_name = "setup dailys field"
+        cmd_args = "furaffinity AF"
+        evt = EventMessage(self.server, self.test_chan, self.test_user, "{} {}".format(cmd_name, cmd_args))
+        evt.split_command_text(cmd_name, cmd_args)
+        spreadsheet = DailysSpreadsheetMock(self.test_user, self.test_chan)
+        # Setup an FA key, doesn't matter if it works
+        udp = UserDataParser()
+        key = FAKeyData("cookie_a", "cookie_b")
+        udp.set_user_data(self.test_user, key)
+        # Create from input
+        field = DailysFAField.create_from_input(evt, spreadsheet)
+        assert field.spreadsheet == spreadsheet
+        assert field.hallo_key_field_id == spreadsheet.test_column_key
 
     def test_create_from_input_with_column_found(self):
         pass
