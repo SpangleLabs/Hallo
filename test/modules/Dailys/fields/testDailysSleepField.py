9d1651273b80829a266c90daa0b1e0ef90869530
joshcoales <joshua@coales.co.uk>
2018-12-23 00:27:28 +0000

    Adding test placeholders for testing DailysSleepField

--- test/modules/Dailys/fields/testDailysSleepField.py
+++ test/modules/Dailys/fields/testDailysSleepField.py
@@ -1,1 +1,49 @@
-# TODO
+import unittest
+
+from modules.Dailys import DailysSleepField
+from test.TestBase import TestBase
+
+
+class DailysSleepFieldTest(TestBase, unittest.TestCase):
+
+    def test_create_from_input_col_found(self):
+        pass
+
+    def test_create_from_input_col_specified(self):
+        pass
+
+    def test_create_from_input_no_col_found(self):
+        pass
+
+    def test_create_from_input_two_col_found(self):
+        pass
+
+    def test_telegram_time(self):
+        pass
+
+    def test_now_time(self):
+        pass
+
+    def test_sleep_wake(self):
+        pass
+
+    def test_sleep_midnight_wake(self):
+        pass
+
+    def test_midnight_sleep_wake(self):
+        pass
+
+    def test_sleep_wake_sleep_wake(self):
+        pass
+
+    def test_two_interruptions(self):
+        pass
+
+    def test_sleep_wake_sleep_midnight_wake(self):
+        pass
+
+    def test_sleep_wake_midnight_sleep_wake(self):
+        pass
+
+    def test_sleep_midnight_wake_sleep_wake(self):
+        pass
