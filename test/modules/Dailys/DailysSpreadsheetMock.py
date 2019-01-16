03242afc01670d4b1ddae20f32ac28f98c31632e
joshcoales <joshua@coales.co.uk>
2019-01-16 19:09:05 +0000

    Adapting DailysSpreadsheetMock to mirror DailysSpreadsheet

--- test/modules/Dailys/DailysSpreadsheetMock.py
+++ test/modules/Dailys/DailysSpreadsheetMock.py
@@ -1,4 +1,3 @@
-from Events import EventMessage
 from modules.Dailys import DailysSpreadsheet
 
 
@@ -7,10 +6,17 @@ class DailysSpreadsheetMock(DailysSpreadsheet):
     test_column_key = "abcdefg"
 
     def __init__(self, user, destination, col_titles=None, saved_data=None):
+        """
+        :type user: User
+        :type destination: Channel | None
+        :type col_titles: dict[str, str]
+        :type saved_data: dict[date, str]
+        """
         super().__init__(user, destination, None)
         self.tagged_columns = []
         self.col_titles = dict() if col_titles is None else col_titles
         self.saved_data = dict() if saved_data is None else saved_data
+        """ :type : dict[date, str]"""
         self.channel_messages = []
 
     def tag_column(self, col_title):
@@ -23,10 +29,20 @@ class DailysSpreadsheetMock(DailysSpreadsheet):
             return None
         return self.col_string_to_num(matches[0])
 
-    def save_field(self, field, data_str, date_modifier=0):
-        self.saved_data[date_modifier] = data_str
+    def save_field(self, field, data_str, data_date):
+        """
+        :type field: DailysField
+        :type data_str: str
+        :type data_date: date
+        """
+        self.saved_data[data_date] = data_str
 
-    def read_field(self, field, date_modifier=0):
-        if date_modifier in self.saved_data:
-            return self.saved_data[date_modifier]
+    def read_field(self, field, data_date):
+        """
+        :type field: DailysField
+        :type data_date: date
+        :rtype: str | None
+        """
+        if data_date in self.saved_data:
+            return self.saved_data[data_date]
         return None
