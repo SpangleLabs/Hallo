cc5a423bb392a17ca3357385341e49432277c4d8
joshcoales <joshua@coales.co.uk>
2019-01-17 23:16:13 +0000

    More mood trigger tests

--- test/modules/Dailys/fields/testDailysMoodField.py
+++ test/modules/Dailys/fields/testDailysMoodField.py
@@ -171,7 +171,53 @@ class DailysMoodFieldTest(TestBase, unittest.TestCase):
         assert all([mood in data_wake[0].text for mood in moods])
 
     def test_trigger_sleep_query(self):
-        pass
+        # Setup
+        spreadsheet = DailysSpreadsheetMock(self.test_user, self.test_chan)
+        # Setup field
+        times = [DailysMoodField.TIME_WAKE, time(14, 0, 0), DailysMoodField.TIME_SLEEP]
+        moods = ["Happiness", "Anger", "Tiredness"]
+        field = DailysMoodField(spreadsheet, spreadsheet.test_column_key, times, moods)
+        # Send message
+        evt_sleep = EventMessage(self.server, self.test_chan, self.test_user, "night")
+        field.passive_trigger(evt_sleep)
+        # Check mood query is sent
+        notif_str = spreadsheet.saved_data[evt_sleep.get_send_time().date()]
+        notif_dict = json.loads(notif_str)
+        assert DailysMoodField.TIME_SLEEP in notif_dict
+        assert "message_id" in notif_dict[DailysMoodField.TIME_SLEEP]
+        # Check query is given
+        data_wake = self.server.get_send_data(1, self.test_chan, EventMessage)
+        assert "how are you feeling" in data_wake[0].text.lower()
+        assert DailysMoodField.TIME_SLEEP in data_wake[0].text
+        assert all([mood in data_wake[0].text for mood in moods])
+
+    def test_trigger_morning_no_query_if_not_in_times(self):
+        # Setup
+        spreadsheet = DailysSpreadsheetMock(self.test_user, self.test_chan)
+        # Setup field
+        times = [time(14, 0, 0), DailysMoodField.TIME_SLEEP]
+        moods = ["Happiness", "Anger", "Tiredness"]
+        field = DailysMoodField(spreadsheet, spreadsheet.test_column_key, times, moods)
+        # Send message
+        evt_wake = EventMessage(self.server, self.test_chan, self.test_user, "morning")
+        field.passive_trigger(evt_wake)
+        # Check mood query is not sent or added to saved data
+        assert evt_wake.get_send_time().date() not in spreadsheet.saved_data
+        self.server.get_send_data(0)
+
+    def test_trigger_sleep_no_query_if_not_in_times(self):
+        # Setup
+        spreadsheet = DailysSpreadsheetMock(self.test_user, self.test_chan)
+        # Setup field
+        times = [DailysMoodField.TIME_WAKE, time(14, 0, 0)]
+        moods = ["Happiness", "Anger", "Tiredness"]
+        field = DailysMoodField(spreadsheet, spreadsheet.test_column_key, times, moods)
+        # Send message
+        evt_sleep = EventMessage(self.server, self.test_chan, self.test_user, "night")
+        field.passive_trigger(evt_sleep)
+        # Check mood query is not sent or added to saved data
+        assert evt_sleep.get_send_time().date() not in spreadsheet.saved_data
+        self.server.get_send_data(0)
 
     def test_trigger_sleep_no_query_if_already_given(self):
         pass
