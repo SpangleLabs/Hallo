a701a2663e0feb1722377fec3133b1a6e735b659
joshcoales <joshua@coales.co.uk>
2019-02-21 22:30:23 +0000

    Mocking out RNG methods for ChooseTest

--- test/modules/Random/testChoose.py
+++ test/modules/Random/testChoose.py
@@ -1,10 +1,34 @@
 import unittest
 
 from Events import EventMessage
+from inc.Commons import Commons
 from test.TestBase import TestBase
 
 
+class MockChooser:
+
+    def __init__(self):
+        self.choice = 0
+        self.last_choices = None
+        self.last_count = None
+
+    def choose(self, choices, count=1):
+        self.last_choices = choices
+        self.last_count = count
+        return choices[self.choice]
+
+
 class ChooseTest(TestBase, unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.chooser = MockChooser()
+        self.old_choice_method = Commons.get_random_choice
+        Commons.get_random_choice = self.chooser.choose
+
+    def tearDown(self):
+        super().tearDown()
+        Commons.get_random_choice = self.old_choice_method
 
     def test_no_options(self):
         self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose"))
@@ -17,93 +41,119 @@ class ChooseTest(TestBase, unittest.TestCase):
         assert "more than 1 thing" in data[0].text.lower(), "Not warning about single option."
 
     def test_x_or_y(self):
-        results = {"x": 0, "y": 0}
-        for _ in range(100):
-            self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or y"))
-            data = self.server.get_send_data(1, self.test_user, EventMessage)
-            assert "i choose" in data[0].text.lower(), "Didn't choose an option"
-            if "\"x\"" in data[0].text.lower():
-                results["x"] += 1
-            elif "\"y\"" in data[0].text.lower():
-                results["y"] += 1
-            else:
-                assert False, "Option chosen wasn't one of the 2 options."
-        assert results["x"] + results["y"] == 100, "Not 100 options were chosen."
-        assert results["x"] < 65, "Chose first option too often."
-        assert results["x"] >= 35, "Chose second option too often."
+        # Set mock value
+        self.chooser.choice = 0
+        # Choose x
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or y"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"x\"" in data[0].text.lower()
+        # Set mock value
+        self.chooser.choice = 1
+        # Choose y
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or y"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"y\"" in data[0].text.lower()
 
     def test_x_comma_y(self):
-        results = {"x": 0, "y": 0}
-        for _ in range(100):
-            self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x, y"))
-            data = self.server.get_send_data(1, self.test_user, EventMessage)
-            assert "i choose" in data[0].text.lower(), "Didn't choose an option"
-            if "\"x\"" in data[0].text.lower():
-                results["x"] += 1
-            elif "\"y\"" in data[0].text.lower():
-                results["y"] += 1
-            else:
-                assert False, "Option chosen wasn't one of the 2 options."
-        assert results["x"] + results["y"] == 100, "Not 100 options were chosen."
-        assert results["x"] < 65, "Chose first option too often."
-        assert results["x"] >= 35, "Chose second option too often."
+        # Set mock value
+        self.chooser.choice = 0
+        # Choose x
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x, y"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"x\"" in data[0].text.lower()
+        # Set mock value
+        self.chooser.choice = 1
+        # Choose y
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x, y"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"y\"" in data[0].text.lower()
 
     def test_x_or_comma_y(self):
-        results = {"x": 0, "y": 0}
-        for _ in range(100):
-            self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or, y"))
-            data = self.server.get_send_data(1, self.test_user, EventMessage)
-            assert "i choose" in data[0].text.lower(), "Didn't choose an option"
-            if "\"x\"" in data[0].text.lower():
-                results["x"] += 1
-            elif "\"y\"" in data[0].text.lower():
-                results["y"] += 1
-            else:
-                assert False, "Option chosen wasn't one of the 2 options."
-        assert results["x"] + results["y"] == 100, "Not 100 options were chosen."
-        assert results["x"] < 65, "Chose first option too often."
-        assert results["x"] >= 35, "Chose second option too often."
+        # Set mock value
+        self.chooser.choice = 0
+        # Choose x
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or, y"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"x\"" in data[0].text.lower()
+        # Set mock value
+        self.chooser.choice = 1
+        # Choose y
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or, y"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"y\"" in data[0].text.lower()
 
     def test_x_y_z(self):
-        results = {"x": 0, "y": 0, "z": 0}
-        for _ in range(100):
-            self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or y or z"))
-            data = self.server.get_send_data(1, self.test_user, EventMessage)
-            assert "i choose" in data[0].text.lower(), "Didn't choose an option"
-            if "\"x\"" in data[0].text.lower():
-                results["x"] += 1
-            elif "\"y\"" in data[0].text.lower():
-                results["y"] += 1
-            elif "\"z\"" in data[0].text.lower():
-                results["z"] += 1
-            else:
-                assert False, "Option chosen wasn't one of the 3 options."
-        assert results["x"] + results["y"] + results["z"] == 100, "Not 100 options were chosen."
-        assert results["x"] < 49, "Chose first option too often."
-        assert results["x"] >= 19, "Chose first option not often enough."
-        assert results["y"] < 49, "Chose second option too often."
-        assert results["y"] >= 19, "Chose second option not often enough."
-        assert results["z"] < 49, "Chose third option too often."
-        assert results["z"] >= 19, "Chose third option not often enough."
+        # Set mock value
+        self.chooser.choice = 0
+        # Choose x
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or y or z"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y", "z"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"x\"" in data[0].text.lower()
+        # Set mock value
+        self.chooser.choice = 1
+        # Choose y
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or y or z"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y", "z"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"y\"" in data[0].text.lower()
+        # Set mock value
+        self.chooser.choice = 2
+        # Choose z
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or y or z"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y", "z"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"z\"" in data[0].text.lower()
 
     def test_multiple_separators(self):
-        results = {"x": 0, "y": 0, "z": 0}
-        for _ in range(100):
-            self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x, y or z"))
-            data = self.server.get_send_data(1, self.test_user, EventMessage)
-            assert "i choose" in data[0].text.lower(), "Didn't choose an option"
-            if "\"x\"" in data[0].text.lower():
-                results["x"] += 1
-            elif "\"y\"" in data[0].text.lower():
-                results["y"] += 1
-            elif "\"z\"" in data[0].text.lower():
-                results["z"] += 1
-            else:
-                assert False, "Option chosen wasn't one of the 3 options."
-        assert results["x"] + results["y"] + results["z"] == 100, "Not 100 options were chosen."
-        assert results["x"] < 49, "Chose first option too often."
-        assert results["x"] >= 19, "Chose first option not often enough."
-        assert results["y"] < 49, "Chose second option too often."
-        assert results["y"] >= 19, "Chose second option not often enough."
-        assert results["z"] < 49, "Chose third option too often."
-        assert results["z"] >= 19, "Chose third option not often enough."
+        # Set mock value
+        self.chooser.choice = 0
+        # Choose x
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x, y or z"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y", "z"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"x\"" in data[0].text.lower()
+        # Set mock value
+        self.chooser.choice = 1
+        # Choose y
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x or y or, z"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y", "z"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"y\"" in data[0].text.lower()
+        # Set mock value
+        self.chooser.choice = 2
+        # Choose z
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "choose x, y or, z"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert self.chooser.last_choices == ["x", "y", "z"]
+        assert self.chooser.last_count == 1
+        assert "i choose" in data[0].text.lower()
+        assert "\"z\"" in data[0].text.lower()
