234428c4ed6c8afc3a1376a874a1fb29241ce0cc
joshcoales <joshua@coales.co.uk>
2019-02-22 10:12:21 +0000

    Pulling eightball responses out from run() method, using get_random_choice

--- modules/Random.py
+++ modules/Random.py
@@ -92,6 +92,13 @@ class EightBall(Function):
     """
     Magic 8 ball. Format: eightball
     """
+    RESPONSES_YES_TOTALLY = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely',
+                             'You may rely on it']
+    RESPONSES_YES_PROBABLY = ['As I see it yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes']
+    RESPONSES_MAYBE = ['Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now',
+                       'Concentrate and ask again']
+    RESPONSES_NO = ["Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good',
+                    'Very doubtful']
 
     def __init__(self):
         """
@@ -106,14 +113,10 @@ class EightBall(Function):
         self.help_docs = "Magic 8 ball. Format: eightball"
 
     def run(self, event):
-        responses = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it']
-        responses += ['As I see it yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes']
-        responses += ['Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now',
-                      'Concentrate and ask again']
-        responses += ["Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good',
-                      'Very doubtful']
-        rand = Commons.get_random_int(0, len(responses) - 1)[0]
-        return event.create_response("{}.".format(responses[rand]))
+        responses = EightBall.RESPONSES_YES_TOTALLY + EightBall.RESPONSES_YES_PROBABLY + \
+                    EightBall.RESPONSES_MAYBE + EightBall.RESPONSES_NO
+        resp = Commons.get_random_choice(responses)[0]
+        return event.create_response("{}.".format(resp))
 
     def get_names(self):
         """Returns the list of names for directly addressing the function"""
