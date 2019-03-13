b97145af42d0ee119858dc8a91cdc15a02aadf7b
joshcoales <joshua@coales.co.uk>
2019-03-13 23:16:27 +0000

    Implementing tests in ConvertTest class

--- test/modules/Convert/testConvert.py
+++ test/modules/Convert/testConvert.py
@@ -1,21 +1,45 @@
 import unittest
 
+import modules.Convert
+from Events import EventMessage
+from test.TestBase import TestBase
 from test.modules.Convert.ConvertFunctionTestBase import ConvertFunctionTestBase
+from test.modules.Convert.testConvertViewRepo import MockMethod
 
 
-class ConvertTest(ConvertFunctionTestBase, unittest.TestCase):
+class ConvertTest(TestBase, unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.output_parse = "{parse called}"
+        self.mock_parse = MockMethod(self.output_parse)
+        self.conv_parse = modules.Convert.Convert.convert_parse
+        modules.Convert.Convert.convert_parse = self.mock_parse.method
+
+    def tearDown(self):
+        modules.Convert.Convert.convert_parse = self.conv_parse
+        super().tearDown()
 
     def test_passive_run(self):
-        assert False
-        pass
+        self.function_dispatcher.dispatch_passive(EventMessage(
+            self.server, self.test_chan, self.test_user,
+            "1 unit1b to unit1a"))
+        data = self.server.get_send_data(1, self.test_chan, EventMessage)
+        assert data[0].text == self.output_parse
+        assert self.mock_parse.arg == ("1 unit1b to unit1a", True)
 
     def test_run(self):
-        assert False
-        pass
+        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "convert 1 unit1b to unit1a"))
+        data = self.server.get_send_data(1, self.test_user, EventMessage)
+        assert data[0].text == self.output_parse
+        assert self.mock_parse.arg == ("1 unit1b to unit1a",)
 
     def test_load_repo(self):
-        assert False
-        pass
+        conv_cls = self.function_dispatcher.get_function_by_name("convert")
+        conv_obj = self.function_dispatcher.get_function_object(conv_cls)
+        assert conv_obj.convert_repo is not None
+        assert isinstance(conv_obj.convert_repo, modules.Convert.ConvertRepo)
+        assert len(conv_obj.convert_repo.type_list) > 0
 
 
 class ConvertConvertParseTest(ConvertFunctionTestBase, unittest.TestCase):
