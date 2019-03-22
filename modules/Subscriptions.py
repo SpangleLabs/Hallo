4b9968716a0053f83601426d7b054d01a83d2042
joshcoales <joshua@coales.co.uk>
2019-03-22 20:18:02 +0000

    Neatening up RedditSub.format_item() a bit more

--- modules/Subscriptions.py
+++ modules/Subscriptions.py
@@ -526,7 +526,7 @@ class RedditSub(Subscription):
             if file_extension == "gifv":
                 url = url[:-4]+"mp4"
             # Make output message
-            output = "Update on /r/{}/ subreddit. \"[{}]({})\" by [u/{}]({}) [direct image]({})".format(
+            output = "Update on /r/{}/ subreddit. \"[{}]({})\" by [u/{}]({})\n[direct image]({})".format(
                 self.subreddit,
                 title,
                 link,
@@ -541,7 +541,7 @@ class RedditSub(Subscription):
         if gfycat_match is not None:
             direct_url = "https://giant.gfycat.com/{}.mp4".format(gfycat_match.group(1))
             # Make output message
-            output = "Update on /r/{}/ subreddit. \"[{}]({})\" by [u/{}]({}) [gfycat]({})".format(
+            output = "Update on /r/{}/ subreddit. \"[{}]({})\" by [u/{}]({})\n[gfycat]({})".format(
                 self.subreddit,
                 title,
                 link,
@@ -559,7 +559,7 @@ class RedditSub(Subscription):
             else:
                 direct_url = item["data"]["secure_media"]["reddit_video"]["fallback_url"]
             # Make output message
-            output = "Update on /r/{}/ subreddit. \"[{}]({})\" by [u/{}]({}) [vreddit]({})".format(
+            output = "Update on /r/{}/ subreddit. \"[{}]({})\" by [u/{}]({})\n[vreddit]({})".format(
                 self.subreddit,
                 title,
                 link,
@@ -570,10 +570,21 @@ class RedditSub(Subscription):
             return output_evt
         # Make output message if the link isn't direct to a media file
         if item["data"]["selftext"] != "":
-            output = "Update on /r/{}/ subreddit. \"{}\" by u/{} {}".format(self.subreddit, title, author, link)
+            output = "Update on /r/{}/ subreddit. \"[{}]({})\" by [u/{}]({})".format(
+                self.subreddit,
+                title,
+                link,
+                author,
+                author_link)
         else:
-            output = "Update on /r/{}/ subreddit. \"{}\" by u/{} {}\n{}".format(
-                self.subreddit, title, author, url, link)
+            output = "Update on /r/{}/ subreddit. \"[{}]({})\" by [u/{}]({})\n{}".format(
+                self.subreddit,
+                title,
+                link,
+                author,
+                author_link,
+                url,
+                link)
         output_evt = EventMessage(self.server, channel, user, output, inbound=False)
         return output_evt
 
