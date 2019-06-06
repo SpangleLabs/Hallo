7693fa290c91cd526bf5fc1b008f7411c49e8b4c
Joshua Coales <joshua@coales.co.uk>
2019-06-06 12:00:04 +0100

    DailysSpreadsheetMock should be storing data as dicts, not strings

--- test/modules/Dailys/DailysSpreadsheetMock.py
+++ test/modules/Dailys/DailysSpreadsheetMock.py
@@ -7,21 +7,21 @@ class DailysSpreadsheetMock(DailysSpreadsheet):
         """
         :type user: User
         :type destination: Channel | None
-        :type saved_data: dict[str, dict[date, str]]
+        :type saved_data: dict[str, dict[date, dict]]
         """
         super().__init__(user, destination, None)
         self.saved_data = dict() if saved_data is None else saved_data
         """ :type : dict[str, dict[date, str]]"""
 
-    def save_field(self, field, data_str, data_date):
+    def save_field(self, field, data, data_date):
         """
         :type field: DailysField
-        :type data_str: str
+        :type data: dict
         :type data_date: date
         """
         if field.type_name not in self.saved_data:
             self.saved_data[field.type_name] = {}
-        self.saved_data[field.type_name][data_date] = data_str
+        self.saved_data[field.type_name][data_date] = data
 
     def read_field(self, field, data_date):
         """
