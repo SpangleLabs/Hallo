882415be72f333e61764a5332e3ff5ced1bc5e9d
joshcoales <joshua@coales.co.uk>
2019-09-19 14:25:31 +0100

    Pulling out the InputParser from Convert module, and renaming from ConvertInputParser

--- test/inc/testInputParser.py
+++ test/inc/testInputParser.py
@@ -1,52 +1,52 @@
 import unittest
 
-from modules.Convert import ConvertInputParser
+from inc.InputParser import InputParser
 
 
 class ConvertInputParserTest(unittest.TestCase):
 
     def test_no_args(self):
-        p = ConvertInputParser("blah blah")
+        p = InputParser("blah blah")
         assert p.remaining_text == "blah blah"
         assert len(p.args_dict) == 0
 
     def test_multiple_simple_args(self):
-        p = ConvertInputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
+        p = InputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
         assert p.remaining_text == "blah blah"
         assert p.args_dict["arg1"] == "val1"
         assert p.args_dict["arg2"] == "val2"
         assert p.args_dict["arg3"] == "val3"
 
     def test_quoted_args_quoted_values(self):
-        p = ConvertInputParser("yo 'base unit'=\"hello world\"")
+        p = InputParser("yo 'base unit'=\"hello world\"")
         assert p.remaining_text == "yo"
         assert p.args_dict["base unit"] == "hello world"
 
     def test_quoted_args_unquoted_values(self):
-        p = ConvertInputParser("yo 'base unit'=hello world")
+        p = InputParser("yo 'base unit'=hello world")
         assert p.remaining_text == "yo world"
         assert p.args_dict["base unit"] == "hello"
 
     def test_unquoted_args_quoted_values(self):
-        p = ConvertInputParser("yo base unit=\"hello world\"")
+        p = InputParser("yo base unit=\"hello world\"")
         assert p.remaining_text == "yo base"
         assert p.args_dict["unit"] == "hello world"
 
     def test_unquoted_args_unquoted_values(self):
-        p = ConvertInputParser("yo base unit=hello world")
+        p = InputParser("yo base unit=hello world")
         assert p.remaining_text == "yo base world"
         assert p.args_dict["unit"] == "hello"
 
     def test_mismatched_quotes(self):
-        p = ConvertInputParser("yo 'base unit\"=\"hello world\"")
+        p = InputParser("yo 'base unit\"=\"hello world\"")
         assert p.remaining_text == "yo 'base"
         assert p.args_dict["unit\""] == "hello world"
-        p = ConvertInputParser("yo 'base unit'=\"hello's world\"")
+        p = InputParser("yo 'base unit'=\"hello's world\"")
         assert p.remaining_text == "yo"
         assert p.args_dict["base unit"] == "hello's world"
 
     def test_all_types(self):
-        p = ConvertInputParser("yo 'base unit'=\"hello world\" arg1='value 1' 'arg 2'=val2 arg3=val3")
+        p = InputParser("yo 'base unit'=\"hello world\" arg1='value 1' 'arg 2'=val2 arg3=val3")
         assert p.remaining_text == "yo"
         assert p.args_dict["base unit"] == "hello world"
         assert p.args_dict["arg1"] == "value 1"
@@ -54,17 +54,17 @@ class ConvertInputParserTest(unittest.TestCase):
         assert p.args_dict["arg3"] == "val3"
 
     def test_remaining_text_start_and_end(self):
-        p = ConvertInputParser("blah blah arg1=val1 arg2=val2 hey")
+        p = InputParser("blah blah arg1=val1 arg2=val2 hey")
         assert p.remaining_text == "blah blah hey"
         assert p.args_dict["arg1"] == "val1"
         assert p.args_dict["arg2"] == "val2"
 
     def test_unstripped_input(self):
-        p = ConvertInputParser("   blah blah ")
+        p = InputParser("   blah blah ")
         assert p.remaining_text == "blah blah"
 
     def test_get_arg_by_names(self):
-        p = ConvertInputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
+        p = InputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
         assert p.remaining_text == "blah blah"
         assert p.get_arg_by_names(["arg2"]) == "val2"
         assert p.args_dict["arg1"] == "val1"
@@ -72,7 +72,7 @@ class ConvertInputParserTest(unittest.TestCase):
         assert p.args_dict["arg3"] == "val3"
 
     def test_get_arg_by_names_no_match(self):
-        p = ConvertInputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
+        p = InputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
         assert p.remaining_text == "blah blah"
         assert p.get_arg_by_names(["arg4"]) is None
         assert p.args_dict["arg1"] == "val1"
@@ -80,7 +80,7 @@ class ConvertInputParserTest(unittest.TestCase):
         assert p.args_dict["arg3"] == "val3"
 
     def test_get_arg_by_names_one_match(self):
-        p = ConvertInputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
+        p = InputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
         assert p.remaining_text == "blah blah"
         assert p.get_arg_by_names(["arg4", "arg5", "arg3"]) == "val3"
         assert p.args_dict["arg1"] == "val1"
@@ -88,7 +88,7 @@ class ConvertInputParserTest(unittest.TestCase):
         assert p.args_dict["arg3"] == "val3"
 
     def test_get_arg_by_names_first_match(self):
-        p = ConvertInputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
+        p = InputParser("blah blah arg1=val1 arg2=val2 arg3=val3")
         assert p.remaining_text == "blah blah"
         assert p.get_arg_by_names(["arg1", "arg2"]) == "val1"
         assert p.args_dict["arg1"] == "val1"
@@ -96,7 +96,7 @@ class ConvertInputParserTest(unittest.TestCase):
         assert p.args_dict["arg3"] == "val3"
 
     def test_parse_string_no_numbers(self):
-        p = ConvertInputParser("blah bloo blee")
+        p = InputParser("blah bloo blee")
         assert p.remaining_text == "blah bloo blee"
         assert len(p.args_dict) == 0
         assert len(p.string_words) == 3
@@ -104,7 +104,7 @@ class ConvertInputParserTest(unittest.TestCase):
         assert p.string_words == ["blah", "bloo", "blee"]
 
     def test_parse_string_all_numbers(self):
-        p = ConvertInputParser("5 421 8916 34.5 -3")
+        p = InputParser("5 421 8916 34.5 -3")
         assert p.remaining_text == "5 421 8916 34.5 -3"
         assert len(p.args_dict) == 0
         assert len(p.string_words) == 0
@@ -112,7 +112,7 @@ class ConvertInputParserTest(unittest.TestCase):
         assert p.number_words == [5, 421, 8916, 34.5, -3]
 
     def test_parse_string_mix_of_numbers_and_args(self):
-        p = ConvertInputParser("blah blah arg1=val1 arg2=val2 5")
+        p = InputParser("blah blah arg1=val1 arg2=val2 5")
         assert p.remaining_text == "blah blah 5"
         assert p.args_dict["arg1"] == "val1"
         assert p.args_dict["arg2"] == "val2"
