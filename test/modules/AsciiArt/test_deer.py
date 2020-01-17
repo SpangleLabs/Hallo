515b4810e3d382dfc07fe5a1c465cc7f34a7b6e8
Joshua Coales <joshua@coales.co.uk>
2020-01-17 18:04:17 +0000

    Creating a hallo_getter fixture, which returns hallo, server, channel and user instance for tests

--- test/modules/AsciiArt/test_deer.py
+++ test/modules/AsciiArt/test_deer.py
@@ -1,13 +1,62 @@
-import unittest
+import time
+from threading import Thread
+
+import pytest
 
 from events import EventMessage
-from test.test_base import TestBase
+from function_dispatcher import FunctionDispatcher
+from hallo import Hallo
+from test.server_mock import ServerMock
 
 
-class DeerTest(TestBase, unittest.TestCase):
+@pytest.fixture
+def hallo_getter():
+    # Create a Hallo
+    hallo = Hallo()
+    hallo.printer.output = lambda *args: None
+    hallo_thread = Thread(target=hallo.start, )
 
-    def test_deer_simple(self):
-        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "deer"))
-        data = self.server.get_send_data()
-        assert "error" not in data[0].text, "Deer output should not produce errors."
-        assert "\n" in data[0].text, "Deer output should be multiple lines."
+    def function(modules=None):
+        if modules is None:
+            modules = {
+                "ascii_art", "bio", "channel_control", "convert", "euler", "hallo_control", "math",
+                "permission_control", "random", "server_control", "silly", "subscriptions"
+            }
+        function_dispatcher = FunctionDispatcher(modules, hallo)
+        hallo.function_dispatcher = function_dispatcher
+        # Create server
+        server = ServerMock(hallo)
+        server.name = "mock-server"
+        hallo.add_server(server)
+        # Start hallo thread
+        hallo_thread.start()
+        # Create test users and channel, and configure them
+        hallo_user = server.get_user_by_address(server.get_nick().lower(), server.get_nick())
+        test_user = server.get_user_by_address("test", "test")
+        test_user.online = True
+        test_chan = server.get_channel_by_address("#test", "#test")
+        test_chan.in_channel = True
+        test_chan.add_user(hallo_user)
+        test_chan.add_user(test_user)
+        # Wait until hallo is open
+        count = 0
+        while not hallo.open:
+            time.sleep(0.01)
+            count += 1
+            assert count < 1000, "Hallo took too long to start."
+            if count > 1000:
+                break
+        # Clear any data in the server
+        server.get_send_data()
+        return hallo, server, test_chan, test_user
+    yield function
+    hallo.close()
+    hallo_thread.join()
+
+
+def test_deer_simple(hallo_getter):
+    hallo, test_server, test_chan, test_user = hallo_getter({"ascii_art"})
+    hallo.function_dispatcher.dispatch(EventMessage(test_server, None, test_user, "deer"))
+    data = test_server.get_send_data()
+    assert "error" not in data[0].text, "Deer output should not produce errors."
+    assert "\n" in data[0].text, "Deer output should be multiple lines."
--- test/modules/AsciiArt/test_train.py
+++ test/modules/AsciiArt/test_train.py
@@ -1,14 +1,11 @@
-import unittest
-
+from test.modules.AsciiArt.test_deer import hallo_getter
 from events import EventMessage
-from test.test_base import TestBase
 
 
-class TrainTest(TestBase, unittest.TestCase):
-
-    def test_train_simple(self):
-        self.function_dispatcher.dispatch(EventMessage(self.server, None, self.test_user, "train"))
-        data = self.server.get_send_data()
-        assert "error" not in data[0].text, "Train output should not produce errors."
-        assert "\n" in data[0].text, "Train output should be multiple lines."
-        assert "chugga chugga" in data[0].text, "Train needs to say chugga chugga."
+def test_train_simple(hallo_getter):
+    hallo, test_server, test_chan, test_user = hallo_getter({"ascii_art"})
+    hallo.function_dispatcher.dispatch(EventMessage(test_server, None, test_user, "train"))
+    data = test_server.get_send_data()
+    assert "error" not in data[0].text, "Train output should not produce errors."
+    assert "\n" in data[0].text, "Train output should be multiple lines."
+    assert "chugga chugga" in data[0].text, "Train needs to say chugga chugga."
