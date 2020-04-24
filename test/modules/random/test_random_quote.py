9b9a9e91a573490b034f8a0e7682ecc5f9f06394
Joshua Coales <joshua@coales.co.uk>
2020-04-24 22:52:30 +0100

    Swapping random quotes API, no need for a mashape key at all now, woo

--- test/modules/random/test_random_quote.py
+++ test/modules/random/test_random_quote.py
@@ -9,10 +9,6 @@ from events import EventMessage
 @pytest.mark.external_integration
 def test_quote(hallo_getter):
     hallo, test_server, test_channel, test_user = hallo_getter({"random"})
-    # Check API key is set
-    if hallo.get_api_key("mashape") is None:
-        # Read from env variable
-        hallo.add_api_key("mashape", os.getenv("test_api_key_mashape"))
     hallo.function_dispatcher.dispatch(
         EventMessage(test_server, None, test_user, "random quote")
     )
@@ -21,18 +17,3 @@ def test_quote(hallo_getter):
     assert (
         quote_regex.match(data[0].text) is not None
     ), "Quote and author not in response."
-
-
-@pytest.mark.external_integration
-def test_quote_no_key(hallo_getter):
-    hallo, test_server, test_channel, test_user = hallo_getter({"random"})
-    # Check there's no API key
-    if hallo.get_api_key("mashape") is not None:
-        del hallo.api_key_list["mashape"]
-    # Test function
-    hallo.function_dispatcher.dispatch(
-        EventMessage(test_server, None, test_user, "random quote")
-    )
-    data = test_server.get_send_data(1, test_user, EventMessage)
-    assert "no api key" in data[0].text.lower(), "Not warning about lack of API key."
-    assert "mashape" in data[0].text.lower(), "Not specifying mashape API."
