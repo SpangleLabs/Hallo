d4f3e91c9a83f7d5f4059f8a261d33e1905da6bb
Joshua Coales <joshua@coales.co.uk>
2020-09-28 17:19:53 +0100

    Neatening up direct embedding of reddit posts

--- hallo/modules/subscriptions/source_reddit.py
+++ hallo/modules/subscriptions/source_reddit.py
@@ -1,11 +1,70 @@
 import re
 from typing import Dict, List, Optional
 
+from bs4 import BeautifulSoup
+
 from hallo.destination import Destination, User, Channel
 from hallo.events import EventMessage, EventMessageWithPhoto
 from hallo.inc.commons import Commons
 import hallo.modules.subscriptions.stream_source
 from hallo.server import Server
+
+
+def _direct_url_gfycat(url: str) -> Optional[str]:
+    gfycat_regex = re.compile(
+        r"(?:https?://)?(?:www\.)?gfycat\.com/([a-z]+)", re.IGNORECASE
+    )
+    gfycat_match = gfycat_regex.match(url)
+    if gfycat_match is None:
+        return None
+    return "https://giant.gfycat.com/{}.mp4".format(gfycat_match.group(1))
+
+
+def _direct_url_vreddit(url: str, item: Dict) -> Optional[str]:
+    vreddit_regex = re.compile(r"https?://v.redd.it/[a-z0-9]+")
+    vreddit_match = vreddit_regex.match(url)
+    if vreddit_match is None:
+        return None
+    if item["data"]["secure_media"] is None:
+        return item["data"]["crosspost_parent_list"][0]["secure_media"]["reddit_video"]["fallback_url"]
+    return item["data"]["secure_media"]["reddit_video"]["fallback_url"]
+
+
+def _direct_url_red(url: str) -> Optional[str]:
+    redgifs_regex = re.compile(r"(?:https?://)?(?:www\.)?redgifs\.com/watch/([a-z]+)", re.IGNORECASE)
+    redgifs_match = redgifs_regex.match(url)
+    if redgifs_match is None:
+        return None
+    page_source = Commons.load_url_string(redgifs_match.group(0))
+    page_soup = BeautifulSoup(page_source, "html.parser")
+    sources = page_soup.select(".video-player-wrapper video source")
+    if not sources:
+        return None
+    best_sources = [
+        source["src"]
+        for source
+        in sources
+        if source["type"] == "video/mp4" and not source["src"].endswith("-mobile.mp4")
+    ]
+    if best_sources:
+        return best_sources[0]
+    mp4_sources = [source["src"] for source in sources if source["type"] == "video/mp4"]
+    if mp4_sources:
+        return mp4_sources[0]
+    return sources[0]["src"]
+
+
+def _get_direct_url(url: str, item: Dict) -> Optional[str]:
+    gfycat_direct = _direct_url_gfycat(url)
+    if gfycat_direct is not None:
+        return gfycat_direct
+    vreddit_direct = _direct_url_vreddit(url, item)
+    if vreddit_direct is not None:
+        return vreddit_direct
+    redgif_direct = _direct_url_red(url)
+    if redgif_direct is not None:
+        return redgif_direct
+    return None
 
 
 class RedditSource(hallo.modules.subscriptions.stream_source.StreamSource[Dict]):
@@ -54,40 +113,13 @@ class RedditSource(hallo.modules.subscriptions.stream_source.StreamSource[Dict])
             )
             output_evt.formatting = EventMessage.Formatting.HTML
             return output_evt
-        # Handle gfycat links as photos
-        gfycat_regex = re.compile(
-            r"(?:https?://)?(?:www\.)?gfycat\.com/([a-z]+)", re.IGNORECASE
-        )
-        gfycat_match = gfycat_regex.match(url)
-        if gfycat_match is not None:
-            direct_url = "https://giant.gfycat.com/{}.mp4".format(gfycat_match.group(1))
-            # Make output message
+        # Handle external sites we can embed
+        direct_url = _get_direct_url(url, item)
+        # If a direct url was found, send that
+        if direct_url is not None:
             output = (
                 f"Update on /r/{Commons.html_escape(self.subreddit)}/ subreddit. "
                 f'"<a href="{link}">{Commons.html_escape(title)}</a>" by '
-                f'"<a href="{author_link}">u/{Commons.html_escape(author)}</a>"'
-            )
-            output_evt = EventMessageWithPhoto(
-                server, channel, user, output, direct_url, inbound=False
-            )
-            output_evt.formatting = EventMessage.Formatting.HTML
-            return output_evt
-        # Handle reddit video links
-        vreddit_regex = re.compile(r"https?://v.redd.it/[a-z0-9]+")
-        vreddit_match = vreddit_regex.match(url)
-        if vreddit_match is not None:
-            if item["data"]["secure_media"] is None:
-                direct_url = item["data"]["crosspost_parent_list"][0]["secure_media"][
-                    "reddit_video"
-                ]["fallback_url"]
-            else:
-                direct_url = item["data"]["secure_media"]["reddit_video"][
-                    "fallback_url"
-                ]
-            # Make output message
-            output = (
-                f"Update on /r/{Commons.html_escape(self.subreddit)}/ subreddit. "
-                f'"<a href="{link}">{link}</a>" by '
                 f'"<a href="{author_link}">u/{Commons.html_escape(author)}</a>"'
             )
             output_evt = EventMessageWithPhoto(
