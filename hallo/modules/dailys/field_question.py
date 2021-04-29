434e6b23aa7bcbb76be0dcf95e2c97e4cfb1b244
Joshua Coales <joshua@coales.co.uk>
2021-04-29 01:04:50 +0100

    Implementing QuestionsField._time_trigger(), and adding AnswerData and AnswerCache classes

--- hallo/modules/dailys/field_question.py
+++ hallo/modules/dailys/field_question.py
@@ -1,16 +1,17 @@
 import datetime
+from threading import RLock
 from typing import Optional, Dict, List
 
 import dateutil.parser
 import isodate
 
 import hallo.modules.dailys.dailys_field
-from hallo.events import EventMessage, EventMinute, Event
+from hallo.events import EventMessage, EventMinute, Event, RawDataTelegramOutbound
 from hallo.modules.dailys.dailys_spreadsheet import DailysSpreadsheet
 
 
 class AnswerOption:
-    type_name = "answer"
+
     def __init__(self, answer: str):
         self.answer = answer
 
@@ -75,8 +76,8 @@ class Question:
         self.allow_custom_answers = allow_custom_answers
         self.answer_options = answer_options or []
         self.deprecation = deprecation
-        self.must_answer = must_answer
-        self.remind_period = remind_period
+        self.must_answer = must_answer  # TODO
+        self.remind_period = remind_period  # TODO
 
     def is_active(self):
         now = datetime.datetime.now(datetime.timezone.utc)
@@ -106,21 +107,105 @@ class Question:
 
 
 class Answer:
-    def __init__(self):
-        self.answered = True
-        self.answer = "Yeah, sure"
-        self.answer_time = datetime.datetime.now()
-        self.asked_time = datetime.datetime.now()
-        self.question_id = "this_week_only"
-        self.question_msg_id = 1234565
+    def __init__(
+            self,
+            question_id: str,
+            asked_time: datetime.datetime,
+            *,
+            answer: Optional[str] = None,
+            answer_time: Optional[datetime.datetime] = None,
+            question_msg_id: Optional[int] = None,
+    ):
+        self.answer = answer
+        self.answer_time = answer_time
+        self.asked_time = asked_time
+        self.question_id = question_id
+        self.question_msg_id = question_msg_id
+
+    @property
+    def answered(self) -> bool:
+        return self.answer is not None
+
+    def same_answer(self, other: 'Answer') -> bool:
+        return self.question_id == other.question_id and self.asked_time == other.asked_time
+
+    @classmethod
+    def from_dict(cls, json_dict: Dict) -> 'Answer':
+        answer_time = None
+        if "answer_time" in json_dict:
+            answer_time = dateutil.parser.parse(json_dict["answer_time"])
+        return Answer(
+            json_dict["question_id"],
+            dateutil.parser.parse(json_dict["asked_time"]),
+            answer=json_dict.get("answer"),
+            answer_time=answer_time,
+            question_msg_id=json_dict.get("q_message_id")
+        )
+
+    def to_dict(self) -> Dict:
+        d = {
+            "question_id": self.question_id,
+            "asked_time": self.asked_time.isoformat()
+        }
+        if self.answer:
+            d["answer"] = self.answer
+        if self.answer_time:
+            d["answer_time"] = self.answer_time.isoformat()
+        if self.question_msg_id:
+            d["q_message_id"] = self.question_msg_id
+        return d
+
+
+class AnswersData:
+    def __init__(self, spreadsheet: DailysSpreadsheet):
+        self.spreadsheet = spreadsheet
+
+    def get_answers_for_date(self, answer_date: datetime.date) -> List[Answer]:
+        date_data = self.spreadsheet.read_path("stats/questions/"+answer_date.isoformat())
+        answer_data = date_data[0]["data"]["answers"]
+        return [Answer.from_dict(d) for d in answer_data]
+
+    def save_answers_for_date(self, answer_date: datetime.date, answers: List[Answer]):
+        date_data = {"answers": [a.to_dict() for a in answers]}
+        self.spreadsheet.save_field(QuestionsField, date_data, answer_date)
+
+    def save_answer(self, answer: Answer) -> None:
+        answer_date = answer.asked_time.date()
+        date_answers = self.get_answers_for_date(answer_date)
+        matching_answer = next([a for a in date_answers if a.same_answer(answer)], None)
+        if matching_answer is not None:
+            date_answers.remove(matching_answer)
+            date_answers.append(answer)
+        else:
+            date_answers.append(answer)
+        self.save_answers_for_date(answer_date, date_answers)
+
+
+class AnswerCache:
+    def __init__(self, data: 'AnswersData'):
+        self.data = data
+        self._cache = {}
+
+    def _populate_answers_for_date(self, answer_date: datetime.date) -> None:
+        answers = self.data.get_answers_for_date(answer_date)
+        self._cache[answer_date] = {}
+        for answer in answers:
+            self._cache[answer_date][answer.question_id][answer.asked_time] = Answer
+
+    def answer_for_question_at_time(self, question: Question, answer_time: datetime.datetime) -> Optional[Answer]:
+        if answer_time.date() not in self._cache:
+            self._populate_answers_for_date(answer_time.date())
+        return self._cache.get(answer_time.date(), {}).get(question.id, {}).get(answer_time)
 
 
 class QuestionsField(hallo.modules.dailys.dailys_field.DailysField):
+    type_name = "questions"
 
     def __init__(self, spreadsheet: DailysSpreadsheet, questions: List[Question]):
         super().__init__(spreadsheet)
         self.questions = questions
-        self.answers_cache = {}
+        self.data = AnswersData(spreadsheet)
+        self.lock = RLock()
 
     @staticmethod
     def create_from_input(event, spreadsheet):
@@ -143,14 +228,41 @@ class QuestionsField(hallo.modules.dailys.dailys_field.DailysField):
 
     def passive_trigger(self, evt: Event) -> Optional[Event]:
         if isinstance(evt, EventMinute):
-            return self.time_trigger(evt)
+            return self._time_trigger()
         if isinstance(evt, EventMessage):
-            return self.msg_trigger(evt)
+            return self._msg_trigger(evt)
 
-    def time_trigger(self, evt: EventMinute) -> Optional[Event]:
-        pass  # TODO
+    def _time_trigger(self) -> None:
+        answer_cache = AnswerCache(self.data)
+        for question in self.questions:
+            if not question.is_active():
+                continue
+            last_time = question.time_pattern.last_time()
+            answer = answer_cache.answer_for_question_at_time(question, last_time)
+            if answer is None:
+                self._ask_question(question, last_time)
 
-    def msg_trigger(self, evt: EventMessage) -> Optional[Event]:
+    def _ask_question(self, question: Question, ask_time: datetime.datetime) -> None:
+        msg = (
+            "I have a question (id={}):\n".format(question.id)
+            + question.question
+        )
+        if question.answer_options:
+            msg += "\n\nHere are some answer options:\n"
+            msg += "\n".join("- {}".format(option.answer) for option in question.answer_options)
+            if question.allow_custom_answers:
+                msg += "\n\nBut custom answers are also allowed."
+        evt = self.message_channel(msg)
+        # Get sent message ID
+        sent_msg_id = None
+        if isinstance(evt.raw_data, RawDataTelegramOutbound):
+            sent_msg_id = evt.raw_data.sent_msg_object.message_id
+        # Create answer object
+        answer = Answer(question.id, ask_time, question_msg_id=sent_msg_id)
+        with self.lock:
+            self.data.save_answer(answer)
+
+    def _msg_trigger(self, evt: EventMessage) -> Optional[Event]:
         pass  # TODO
 
     def to_json(self):
