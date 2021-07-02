7148c91b00ac5c42e9c0438276733c78bd4ba50f
Joshua Coales <joshua@coales.co.uk>
2021-07-03 00:44:32 +0100

    Swapping E621TaggingSource over to Yippi client

--- hallo/modules/subscriptions/source_e621_tagging.py
+++ hallo/modules/subscriptions/source_e621_tagging.py
@@ -1,11 +1,14 @@
 from typing import List, Optional, Dict
 
+from yippi import Post, Rating
+
 import hallo.modules.subscriptions.subscription_exception
 from hallo.destination import Channel, User, Destination
 from hallo.events import EventMessage, EventMessageWithPhoto
 from hallo.inc.input_parser import InputParser
 import hallo.modules.subscriptions.source_e621
 import hallo.modules.subscriptions.stream_source
+import hallo.modules.subscriptions.common_e6_key
 from hallo.server import Server
 
 
@@ -16,10 +19,12 @@ class E621TaggingSource(hallo.modules.subscriptions.source_e621.E621Source):
     def __init__(
             self,
             search: str,
+            e6_client: hallo.modules.subscriptions.common_e6_key.E6ClientCommon,
+            e6_key: hallo.modules.subscriptions.common_e6_key.E6Key,
             tags: List[str],
             last_keys: Optional[List[hallo.modules.subscriptions.stream_source.Key]] = None
     ):
-        super().__init__(search, last_keys)
+        super().__init__(search, e6_client, e6_key, last_keys)
         self.tags: List[str] = tags
 
     @classmethod
@@ -55,7 +60,9 @@ class E621TaggingSource(hallo.modules.subscriptions.source_e621.E621Source):
                     'You need to specify a search term with search="search term" and '
                     'tags to watch with tags="tags to watch"'
                 )
-        return E621TaggingSource(search, tags)
+        e6_client = sub_repo.get_common_config_by_type(hallo.modules.subscriptions.common_e6_key.E6ClientCommon)
+        e6_key = hallo.modules.subscriptions.source_e621.e6_key_from_input(user, sub_repo)
+        return E621TaggingSource(search, e6_client, e6_key, tags)
 
     @property
     def title(self) -> str:
@@ -66,37 +73,46 @@ class E621TaggingSource(hallo.modules.subscriptions.source_e621.E621Source):
             server: Server,
             channel: Optional[Channel],
             user: Optional[User],
-            item: Dict
+            item: Post
     ) -> EventMessage:
-        link = f"https://e621.net/posts/{item['id']}"
+        link = f"https://e621.net/posts/{item.id}"
         # Create rating string
-        rating_dict = {"e": "(Explicit)", "q": "(Questionable)", "s": "(Safe)"}
-        rating = rating_dict.get(item["rating"], "(Unknown)")
+        rating_dict = {Rating.EXPLICIT: "(Explicit)", Rating.QUESTIONABLE: "(Questionable)", Rating.SAFE: "(Safe)"}
+        rating = rating_dict.get(item.rating, "(Unknown)")
         # Check tags
-        post_tags = [tag for tag_list in item["tags"].values() for tag in tag_list]
+        post_tags = [tag for tag_list in item.tags.values() for tag in tag_list]
         tag_results = {tag: tag in post_tags for tag in self.tags}
         tag_output = [f"{tag}: {val}" for tag, val in tag_results.items()]
         # Construct output
         output = f'Update on "{self.search}" tagging e621 search. {link} {rating}.\nWatched tags: {tag_output}'
-        if item["file"]["ext"] in ["swf", "webm"] or item["file"]["url"] is None:
+        if item.file["ext"] in ["swf", "webm"] or item.file["url"] is None:
             return EventMessage(server, channel, user, output, inbound=False)
-        image_url = item["file"]["url"]
+        image_url = item.file["url"]
         return EventMessageWithPhoto(
             server, channel, user, output, image_url, inbound=False
         )
 
     @classmethod
     def from_json(cls, json_data: Dict, destination: Destination, sub_repo) -> 'E621TaggingSource':
+        user_addr = json_data["e621_user_address"]
+        e6_key = hallo.modules.subscriptions.source_e621.e6_key_from_json(user_addr, destination.server, sub_repo)
+        e6_client = sub_repo.get_common_config_by_type(hallo.modules.subscriptions.common_e6_key.E6ClientCommon)
         return E621TaggingSource(
             json_data["search"],
+            e6_client,
+            e6_key,
             json_data["tags"],
             json_data["last_keys"]
         )
 
     def to_json(self) -> Dict:
+        user_addr = None
+        if self.e6_key:
+            user_addr = self.e6_key.user.address
         return {
             "type": self.type_name,
             "last_keys": self.last_keys,
             "search": self.search,
-            "tags": self.tags
+            "tags": self.tags,
+            "e621_user_address": user_addr
         }
