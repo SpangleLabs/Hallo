51a3c861aa5fc57ce990ce8ebb7474155eea5cc3
Joshua Coales <joshua@coales.co.uk>
2021-08-02 18:21:27 +0100

    Simplifying subscription add commands

--- hallo/modules/subscriptions/subscription_add.py
+++ hallo/modules/subscriptions/subscription_add.py
@@ -22,16 +22,11 @@ class SubscriptionAdd(Function):
         self.help_name = "add subscription"
         # Names which can be used to address the function
         name_templates = {
-            "{0} {1}",
-            "{1} {0}",
-            "{1} {0} {2}",
-            "{1} {2} {0}",
-            "{2} {0} {1}",
-            "{0} {2} {1}",
+            "{add} {source} {sub}"
         }
         self.names = set(
             [
-                template.format(name, add, sub)
+                template.format(add=add, source=name, sub=sub)
                 for name in hallo.modules.subscriptions.subscription_factory.SubscriptionFactory.get_source_names()
                 for template in name_templates
                 for add in self.add_words
