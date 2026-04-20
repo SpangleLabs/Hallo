93a68aec5d019208ca791d8fa90a6cf1c06c95fa
Roselyn Coales <github@spangle.org.uk>
2026-04-20 23:56:17 +0100

    Swap channel control functions from using Server.sync_send() to using Server.send()

--- hallo/modules/channel_control/kick.py
+++ hallo/modules/channel_control/kick.py
@@ -1,3 +1,4 @@
+from hallo.destination import Channel, User
 from hallo.events import EventKick
 from hallo.function import Function
 import hallo.modules.channel_control.channel_control
@@ -44,24 +45,18 @@ class Kick(Function):
                 channel = server_obj.get_channel_by_name(event.command_args)
                 if channel is None:
                     return event.create_response(
-                        "Error, {} is not known on {}.".format(
-                            event.command_args, server_obj.name
-                        )
+                        f"Error, {event.command_args} is not known on {server_obj.name}."
                     )
-                return event.create_response(self.send_kick(channel, event.user))
+                return event.create_response(await self.send_kick(channel, event.user))
             # See if it's a channel that hallo is in
             test_channel = server_obj.get_channel_by_name(event.command_args)
             if test_channel is not None and test_channel.in_channel:
-                return event.create_response(self.send_kick(test_channel, event.user))
+                return event.create_response(await self.send_kick(test_channel, event.user))
             # Argument must be a user?
             target_user = server_obj.get_user_by_name(event.command_args)
             if target_user is None:
-                return event.create_response(
-                    "Error, {} is not known on {}.".format(
-                        event.command_args, server_obj.name
-                    )
-                )
-            return event.create_response(self.send_kick(event.channel, target_user))
+                return event.create_response(f"Error, {event.command_args} is not known on {server_obj.name}.")
+            return event.create_response(await self.send_kick(event.channel, target_user))
         if len(line_split) == 2:
             # If message was in private message, it's either channel and user, user and channel or channel and message
             if event.channel is None:
@@ -74,28 +69,20 @@ class Kick(Function):
                             and target_channel.is_user_in_channel(target_user)
                         ):
                             return event.create_response(
-                                self.send_kick(target_channel, target_user)
+                                await self.send_kick(target_channel, target_user)
                             )
                         return event.create_response(
-                            self.send_kick(target_channel, event.user, line_split[1])
+                            await self.send_kick(target_channel, event.user, line_split[1])
                         )
                     return event.create_response("Error, I am not in that channel.")
                 target_user = server_obj.get_user_by_name(line_split[0])
                 if target_user is None:
-                    return event.create_response(
-                        "Error, {} is not known on {}.".format(
-                            line_split[0], server_obj.name
-                        )
-                    )
+                    return event.create_response(f"Error, {line_split[0]} is not known on {server_obj.name}.")
                 target_channel = server_obj.get_channel_by_name(line_split[1])
                 if target_channel is None:
-                    return event.create_response(
-                        "Error, {} is not known on {}.".format(
-                            line_split[1], server_obj.name
-                        )
-                    )
+                    return event.create_response(f"Error, {line_split[1]} is not known on {server_obj.name}.")
                 return event.create_response(
-                    self.send_kick(target_channel, target_user)
+                    await self.send_kick(target_channel, target_user)
                 )
             # If 2 arguments, try with first argument as channel
             target_channel = server_obj.get_channel_by_name(line_split[0])
@@ -105,26 +92,22 @@ class Kick(Function):
                     target_user
                 ):
                     return event.create_response(
-                        self.send_kick(target_channel, target_user)
+                        await self.send_kick(target_channel, target_user)
                     )
                 return event.create_response(
-                    self.send_kick(target_channel, event.user, line_split[1])
+                    await self.send_kick(target_channel, event.user, line_split[1])
                 )
             # 2 args, try with second argument as channel
             target_user = server_obj.get_user_by_name(line_split[0])
             if target_user is None:
-                return event.create_response(
-                    "Error, {} is not known on {}.".format(
-                        line_split[0], server_obj.name
-                    )
-                )
+                return event.create_response(f"Error, {line_split[0]} is not known on {server_obj.name}.")
             target_channel = server_obj.get_channel_by_name(line_split[1])
             if target_channel is not None and target_channel.in_channel:
                 return event.create_response(
-                    self.send_kick(target_channel, target_user)
+                    await self.send_kick(target_channel, target_user)
                 )
             return event.create_response(
-                self.send_kick(event.channel, target_user, line_split[1])
+                await self.send_kick(event.channel, target_user, line_split[1])
             )
         # If message was in private message, it's either channel, user and message or user, channel and message or
         # channel and message
@@ -137,12 +120,12 @@ class Kick(Function):
                         target_user
                     ):
                         return event.create_response(
-                            self.send_kick(
+                            await self.send_kick(
                                 target_channel, target_user, " ".join(line_split[2:])
                             )
                         )
                     return event.create_response(
-                        self.send_kick(
+                        await self.send_kick(
                             target_channel, event.user, " ".join(line_split[1:])
                         )
                     )
@@ -150,19 +133,15 @@ class Kick(Function):
             target_user = server_obj.get_user_by_name(line_split[0])
             if target_user is None:
                 return event.create_response(
-                    "Error, {} is not known on {}.".format(
-                        line_split[0], server_obj.name
-                    )
+                    f"Error, {line_split[0]} is not known on {server_obj.name}."
                 )
             target_channel = server_obj.get_channel_by_name(line_split[1])
             if target_channel is None:
                 return event.create_response(
-                    "Error, {} is not known on {}.".format(
-                        line_split[1], server_obj.name
-                    )
+                    f"Error, {line_split[1]} is not known on {server_obj.name}."
                 )
             return event.create_response(
-                self.send_kick(target_channel, target_user, " ".join(line_split[2:]))
+                await self.send_kick(target_channel, target_user, " ".join(line_split[2:]))
             )
         # If more than 2 arguments, determine which of the first 2 is channel/user, the rest is a message.
         target_channel = server_obj.get_channel_by_name(line_split[0])
@@ -172,54 +151,48 @@ class Kick(Function):
                 target_user
             ):
                 return event.create_response(
-                    self.send_kick(
+                    await self.send_kick(
                         target_channel, target_user, " ".join(line_split[2:])
                     )
                 )
             return event.create_response(
-                self.send_kick(target_channel, event.user, " ".join(line_split[1:]))
+                await self.send_kick(target_channel, event.user, " ".join(line_split[1:]))
             )
         # 2 args, try with second argument as channel
         target_user = server_obj.get_user_by_name(line_split[0])
         if target_user is None:
             return event.create_response(
-                "Error, {} is not known on {}.".format(line_split[0], server_obj.name)
+                f"Error, {line_split[0]} is not known on {server_obj.name}."
             )
         target_channel = server_obj.get_channel_by_name(line_split[1])
         if target_channel is not None and target_channel.in_channel:
             return event.create_response(
-                self.send_kick(target_channel, target_user, " ".join(line_split[2:]))
+                await self.send_kick(target_channel, target_user, " ".join(line_split[2:]))
             )
         return event.create_response(
-            self.send_kick(event.channel, target_user, " ".join(line_split[1:]))
+            await self.send_kick(event.channel, target_user, " ".join(line_split[1:]))
         )
 
-    def send_kick(self, channel, user, message=""):
+    async def send_kick(self, channel: Channel, user: User, message: str = "") -> str:
         """
         Sends an invite to a specified user to join a given channel.
         :param channel: Channel to invite target to
-        :type channel: destination.Channel
         :param user: User to invite to channel
-        :type user: destination.User
         :param message: Kick message to send
-        :type message: str
         :return: Response to send to requester
-        :rtype: str
         """
         # Check if in channel
         if not channel.in_channel:
             return "Error, I'm not in that channel."
         # Check if user is in channel
         if user not in channel.get_user_list():
-            return "Error, {} is not in {}.".format(user.name, channel.name)
+            return f"Error, {user.name} is not in {channel.name}."
         # Check if hallo has op in channel
         if not hallo.modules.channel_control.channel_control.hallo_has_op(channel):
-            return "Error, I don't have power to kick users from {}.".format(
-                channel.name
-            )
+            return f"Error, I don't have power to kick users from {channel.name}."
         # Send invite
         kick_evt = EventKick(
             channel.server, channel, None, user, message, inbound=False
         )
-        channel.server.send_sync(kick_evt)
-        return "Kicked {} from {}.".format(user.name, channel.name)
+        await channel.server.send(kick_evt)
+        return f"Kicked {user.name} from {channel.name}."
--- hallo/modules/channel_control/mute.py
+++ hallo/modules/channel_control/mute.py
@@ -37,35 +37,29 @@ class Mute(Function):
                 return event.create_response(
                     "Error, you can't set mute on a private message."
                 )
-            return event.create_response(self.mute_channel(event.channel))
+            return event.create_response(await self.mute_channel(event.channel))
         # Get channel from user input
         target_channel = server_obj.get_channel_by_name(event.command_args.strip())
         if target_channel is None:
-            return event.create_response(
-                "Error, {} is not known on {}.".format(
-                    event.command_args.strip(), server_obj.name
-                )
-            )
-        return event.create_response(self.mute_channel(target_channel))
+            return event.create_response(f"Error, {event.command_args.strip()} is not known on {server_obj.name}.")
+        return event.create_response(await self.mute_channel(target_channel))
 
-    def mute_channel(self, channel):
+    async def mute_channel(self, channel: Channel) -> str:
         """
         Sets mute on a given channel.
         :param channel: Channel to mute
-        :type channel: destination.Channel
         :return: Response to send to requester
-        :rtype: str
         """
         # Check if in channel
         if not channel.in_channel:
             return "Error, I'm not in that channel."
         # Check if hallo has op in channel
         if not hallo.modules.channel_control.channel_control.hallo_has_op(channel):
-            return "Error, I don't have power to mute {}.".format(channel.name)
+            return f"Error, I don't have power to mute {channel.name}."
         # Send invite
         mode_evt = EventMode(channel.server, channel, None, "+m", inbound=False)
-        channel.server.send_sync(mode_evt)
-        return "Set mute in {}.".format(channel.name)
+        await channel.server.send(mode_evt)
+        return f"Set mute in {channel.name}."
 
 
 class UnMute(Function):
@@ -85,7 +79,7 @@ class UnMute(Function):
         # Help documentation, if it's just a single line, can be set here
         self.help_docs = "Unmutes a given channel or current channel if none is given. Format: unmute <channel>"
 
-    def run(self, event):
+    async def run(self, event):
         # Get server object
         server_obj = event.server
         # If server isn't IRC type, we can't unmute channels
@@ -103,28 +97,22 @@ class UnMute(Function):
         # Get channel from user input
         target_channel = server_obj.get_channel_by_name(event.command_args.strip())
         if target_channel is None:
-            return event.create_response(
-                "Error, {} is not known on {}.".format(
-                    event.command_args.strip(), server_obj.name
-                )
-            )
+            return event.create_response(f"Error, {event.command_args.strip()} is not known on {server_obj.name}.")
         return event.create_response(self.unmute_channel(target_channel))
 
-    def unmute_channel(self, channel):
+    async def unmute_channel(self, channel: Channel) -> str:
         """
         Sets mute on a given channel.
         :param channel: Channel to mute
-        :type channel: destination.Channel
         :return: Response to send to requester
-        :rtype: str
         """
         # Check if in channel
         if not channel.in_channel:
             return "Error, I'm not in that channel."
         # Check if hallo has op in channel
         if not hallo.modules.channel_control.channel_control.hallo_has_op(channel):
-            return "Error, I don't have power to unmute {}.".format(channel.name)
+            return f"Error, I don't have power to unmute {channel.name}."
         # Send invite
         mode_evt = EventMode(channel.server, channel, None, "-m", inbound=False)
-        channel.server.send_sync(mode_evt)
-        return "Unset mute in {}.".format(channel.name)
+        await channel.server.send(mode_evt)
+        return f"Unset mute in {channel.name}."
