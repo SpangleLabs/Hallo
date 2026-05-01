85f1e55ba793a6520448f4189d97551cf2696438
Roselyn Coales <github@spangle.org.uk>
2026-05-01 09:43:10 +0100

    Neatening up imports and string formatting in DailysSpreadsheet

--- hallo/modules/dailys/dailys_spreadsheet.py
+++ hallo/modules/dailys/dailys_spreadsheet.py
@@ -1,14 +1,13 @@
 from typing import TYPE_CHECKING
 import datetime
 
-import hallo.modules.dailys.dailys_field_factory
 from hallo.inc.commons import Commons
-import hallo.modules.dailys.dailys_field
+from hallo.modules.dailys.dailys_field import DailysException, DailysField
+from hallo.modules.dailys.dailys_field_factory import DailysFieldFactory
 
 if TYPE_CHECKING:
     from hallo.destination import Destination, User
     from hallo.hallo import Hallo
-    from hallo.modules.dailys.dailys_field import DailysField
 
 
 class DailysSpreadsheet:
@@ -29,7 +28,7 @@ class DailysSpreadsheet:
         Save given data in a specified column for the current date row.
         """
         if dailys_field.type_name is None:
-            raise hallo.modules.dailys.dailys_field.DailysException("Cannot write to unassigned dailys field")
+            raise DailysException("Cannot write to unassigned dailys field")
         headers = None
         if self.dailys_key is not None:
             headers = [["Authorization", self.dailys_key]]
@@ -46,20 +45,15 @@ class DailysSpreadsheet:
         headers = None
         if self.dailys_key is not None:
             headers = [["Authorization", self.dailys_key]]
-        return await Commons.load_url_json(
-            "{}/{}".format(
-                self.dailys_url, path
-            ),
-            headers
-        )
+        return await Commons.load_url_json(f"{self.dailys_url}/{path}", headers)
 
     async def read_field(self, dailys_field: 'DailysField', data_date: datetime.datetime) -> dict | None:
         """
         Save given data in a specified column for the current date row.
         """
         if dailys_field.type_name is None:
-            raise hallo.modules.dailys.dailys_field.DailysException("Cannot read from unassigned dailys field")
-        data = await self.read_path("stats/{}/{}/".format(dailys_field.type_name, data_date.isoformat()))
+            raise DailysException("Cannot read from unassigned dailys field")
+        data = await self.read_path(f"stats/{dailys_field.type_name}/{data_date.isoformat()}/")
         if len(data) == 0:
             return None
         return data[0]["data"]
@@ -82,30 +76,22 @@ class DailysSpreadsheet:
     async def from_json(json_obj: dict, hallo_obj: Hallo) -> 'DailysSpreadsheet':
         server = hallo_obj.get_server_by_name(json_obj["server_name"])
         if server is None:
-            raise hallo.modules.dailys.dailys_field.DailysException(
-                'Could not find server with name "{}"'.format(json_obj["server"])
-            )
+            raise DailysException(f'Could not find server with name "{json_obj["server"]}"')
         user = server.get_user_by_address(json_obj["user_address"])
         if user is None:
-            raise hallo.modules.dailys.dailys_field.DailysException(
-                'Could not find user with address "{}" on server "{}"'.format(
-                    json_obj["user_address"], json_obj["server"]
-                )
+            raise DailysException(
+                f'Could not find user with address "{json_obj["user_address"]}" on server "{json_obj["server"]}"'
             )
         dest_chan = None
         if "dest_address" in json_obj:
             dest_chan = server.get_channel_by_address(json_obj["dest_address"])
             if dest_chan is None:
-                raise hallo.modules.dailys.dailys_field.DailysException(
-                    'Could not find channel with address "{}" on server "{}"'.format(
-                        json_obj["dest_address"], json_obj["server"]
-                    )
+                raise DailysException(
+                    f'Could not find channel with address "{json_obj["dest_address"]}" on server "{json_obj["server"]}"'
                 )
         dailys_url = json_obj["dailys_url"]
         dailys_key = json_obj.get("dailys_key")
         new_spreadsheet = DailysSpreadsheet(user, dest_chan, dailys_url, dailys_key)
         for field_json in json_obj["fields"]:
-            new_spreadsheet.add_field(
-                await hallo.modules.dailys.dailys_field_factory.DailysFieldFactory.from_json(field_json, new_spreadsheet)
-            )
+            new_spreadsheet.add_field(await DailysFieldFactory.from_json(field_json, new_spreadsheet))
         return new_spreadsheet
